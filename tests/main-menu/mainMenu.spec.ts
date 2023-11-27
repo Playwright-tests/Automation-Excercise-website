4dec350bcee4ae23b4c7f8983dbcc7403e7120be
aksi1984 <pablo_xmen@op.pl>
2023-11-27 23:56:48 +0100

    Created tests for main menu

---
+++ tests/main-menu/mainMenu.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from "../../fixtures/mainMenu.spec";
+import { getLinkData } from "../../helpers/link.spec";
+
+const links = getLinkData('mainMenuLinks');
+
+test.describe('Main menu links',async () => {
+    
+    for(const link of links) {
+
+        test('Clicking the link with id: "' + link.linkText + '"',async ({mainMenu, page}) => {
+            
+            await test.step('Click the link',async () => {
+                
+                await mainMenu.clickLink(link.linkText);
+            })
+
+            await expect(page).toHaveURL(link.url);
+        })
+    }
+})
