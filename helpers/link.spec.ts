c6a4b234417a65e9b8f3e332390954e239906f91
aksi1984 <pablo_xmen@op.pl>
2023-11-27 16:39:41 +0100

    Created the function

---
+++ helpers/link.spec.ts
@@ -0,0 +1,19 @@
+import { getJSONArray } from "./JSONArray.spec";
+
+export function getLinkData(node: string) {
+
+    let JSONArray = getJSONArray('URLs');
+    let links: { linkText: string, url: string }[] = [];
+
+    for(const data of JSONArray[node]) {
+
+        let temp = {
+            linkText: data.linkText,
+            url: data.pageURL
+        }
+
+        links.push(temp);
+    }
+
+    return links;
+}
