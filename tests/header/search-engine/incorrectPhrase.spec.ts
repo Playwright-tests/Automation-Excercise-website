e168e77adbbd9ed24aa2da1ef4d10a43d30c479c
aksi1984 <pablo_xmen@op.pl>
2023-11-27 18:06:11 +0100

    Created tests

---
+++ tests/header/search-engine/incorrectPhrase.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from "../../../fixtures/header.spec";
+import { getArrayData } from "../../../helpers/arrayData.spec";
+import { SearchResultsPage } from "../../../page-object/base/search-results/SearchResultsPage.spec";
+import { steps } from "./steps.spec";
+
+const phrases = getArrayData('searchEngine', 'incorrectPhrase');
+
+test.describe('Search product using an incorrect phrase',async () => {
+    
+    for(const phrase of phrases) {
+
+        test('Typing the "' + phrase + '" as the correct phrase',async ({header, page}) => {
+            
+            await steps(await header.getSearchEngine(), phrase);
+
+            const searchResultsPage = new SearchResultsPage(page);
+
+            expect(await (await searchResultsPage.getFoundMessageLocator()).isVisible()).toBeFalsy();
+            expect(await (await searchResultsPage.getNothingFoundLocator()).isVisible()).toBeTruthy();
+        })
+    }
+})
