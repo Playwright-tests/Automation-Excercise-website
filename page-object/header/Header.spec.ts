07eb7cde602aa9975edfa0aa54d193fd8cd4d5e8
aksi1984 <pablo_xmen@op.pl>
2023-11-27 11:51:55 +0100

    Created the Header class

---
+++ page-object/header/Header.spec.ts
@@ -0,0 +1,36 @@
+import { Locator, Page } from "@playwright/test";
+import { BasePage } from "../base/BasePage.spec";
+import { SearchEngine } from "./SearchEngine.spec";
+
+export class Header extends BasePage {
+
+    readonly accountButton: Locator;
+    readonly shoppingCartButton: Locator;
+
+    private searchEngine: SearchEngine;
+
+    constructor(page: Page) {
+
+        super(page);
+
+        this.accountButton = page.locator('li').filter({hasText: 'Account'});
+        this.shoppingCartButton = page.locator('li').filter({hasText: 'My Cart - zł'});
+
+        this.searchEngine = new SearchEngine(page);
+    }
+
+    async clickAccountButton() {
+
+        await this.accountButton.click();
+    }
+
+    async clickShoppingCartButton() {
+
+        await this.shoppingCartButton.click();
+    }
+
+    async getSearchEngine() {
+
+        return this.searchEngine;
+    }
+}
