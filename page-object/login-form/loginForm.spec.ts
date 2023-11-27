d91af49897d1caffa59e64f9fe4cb6c4fa8c66af
aksi1984 <pablo_xmen@op.pl>
2023-11-27 11:53:37 +0100

    Created the LoginForm class

---
+++ page-object/login-form/loginForm.spec.ts
@@ -0,0 +1,40 @@
+import { Locator, Page } from "@playwright/test";
+import { BasePage } from "../base/BasePage.spec";
+
+export class LoginForm extends BasePage {
+
+    readonly usernameField: Locator;
+    readonly passwordField: Locator;
+    readonly loginButton: Locator;
+    readonly errorMessage: Locator;
+
+    constructor(page: Page) {
+
+        super(page);
+
+        this.usernameField = page.locator('#username');
+        this.passwordField = page.locator('#password');
+        this.loginButton = page.getByRole('button', {name: 'login'});
+        this.errorMessage = page.locator('.woocommerce-error');
+    }
+
+    async setUsername(username: string) {
+
+        await this.usernameField.fill(username);
+    }
+
+    async setPassword(password: string) {
+
+        await this.passwordField.fill(password);
+    }
+
+    async clickLoginButton() {
+
+        await this.loginButton.click();
+    }
+
+    async getError() {
+
+        await this.errorMessage.textContent();
+    }
+}
