082e6a3d3c247eb332e5e9c03088c70102ebbf75
aksi1984 <pablo_xmen@op.pl>
2023-11-28 23:32:25 +0100

    Created the fixture

---
+++ fixtures/thumbnail.spec.ts
@@ -0,0 +1,30 @@
+import { test as base } from "@playwright/test";
+import { ThumbnailType } from "../enums/thumbnailType.spec";
+import { ThumbnailProvider } from "../providers/thumbnails/thumbnailProvider.spec";
+import { ThumbnailGenerator } from "../providers/thumbnails/thumbnailGenerator.spec";
+
+export type ProductThumbnailFixture = {
+
+    thumbnailGenerator: ThumbnailGenerator
+}
+
+export type ThumbnailTypeFixture = {
+
+    thumbnailType: ThumbnailType
+}
+
+
+export const test = base.extend<ProductThumbnailFixture & ThumbnailTypeFixture>({
+
+    thumbnailType: [ThumbnailType.Product, {option: true}],
+
+    thumbnailGenerator:async ({thumbnailType, page}, use) => {
+        
+        const thumbnailGenerator = ThumbnailProvider.getGenerator(thumbnailType);
+
+        await page.goto('https://skleptest.pl/');
+        await use(thumbnailGenerator);
+    }
+})
+
+export { expect } from "@playwright/test";
