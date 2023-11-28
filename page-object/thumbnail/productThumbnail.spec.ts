465182f0b202562bf1f0dcd69878701a3a2983f6
aksi1984 <pablo_xmen@op.pl>
2023-11-28 08:54:33 +0100

    Created the ProductThumbnail class

---
+++ page-object/thumbnail/productThumbnail.spec.ts
@@ -0,0 +1,50 @@
+import { Locator, Page } from "@playwright/test";
+import { BasePage } from "../base/BasePage.spec";
+import { Thumbnail } from "./thumbnail.spec";
+
+export class ProductThumbnail extends BasePage implements Thumbnail {
+
+    private link: Locator;
+    private price: Locator;
+    private addToCartButton: Locator;
+
+    constructor(page: Page) {
+
+        super(page);
+    }
+
+    setLink(link: Locator): void {
+        
+        this.link = link;
+    }
+
+    setPrice(price: Locator) : void {
+
+        this.price = price;
+    }
+
+    setAddToCartButton(button: Locator) : void {
+
+        this.addToCartButton = button;
+    }
+
+    async getLinkText() {
+
+        return await this.link.textContent();
+    }
+
+    async getPrice() {
+
+        return await this.price.textContent();
+    }
+
+    async clickLink() {
+        
+        await this.link.click();
+    }
+
+    async clickAddToCartButton() {
+
+        await this.addToCartButton.click();
+    }
+}
