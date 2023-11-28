4ca46cd15f472d3e4390b4bb61cfe668d64ac23b
aksi1984 <pablo_xmen@op.pl>
2023-11-28 07:58:30 +0100

    Added the DropdownList class

--- page-object/main-menu/mainMenu.spec.ts
+++ page-object/main-menu/mainMenu.spec.ts
@@ -1,15 +1,25 @@
 import { Page } from "@playwright/test";
 import { BasePage } from "../base/BasePage.spec";
+import { DropdownList } from "./dropdownList.spec";
 
 export class MainMenu extends BasePage {
+
+    readonly dropdownList: DropdownList;
 
     constructor(page: Page) {
 
         super(page);
+
+        this.dropdownList = new DropdownList(page);
     }
 
     async clickLink(id: string) {
 
         await (await this.getPage()).locator('#' + id).click();
+    }
+
+    async getDropdownList() {
+
+        return this.dropdownList;
     }
 }
