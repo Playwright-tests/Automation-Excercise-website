ab3fa29d4b7ef719a582ab2be1fb194f1d676d07
aksi1984 <pablo_xmen@op.pl>
2023-11-28 08:01:31 +0100

    Created tests for drop-down list

---
+++ tests/main-menu/dropdownList.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "../../fixtures/mainMenu.spec";
+import { getLinkData } from "../../helpers/link.spec";
+
+const links = getLinkData('mainMenuDropdownList');
+
+test.describe('Main menu drop-down list links',async () => {
+    
+    for(const link of links) {
+
+        test('Clicking the drop-down list link with id: "' + link.linkText,async ({mainMenu, page}) => {
+            
+            await test.step('Hover over the main menu drop-down list parent',async () => {
+                
+                await (await mainMenu.getDropdownList()).hoverParent();
+            })
+
+            await test.step('Click the link',async () => {
+                
+                await (await mainMenu.getDropdownList()).clickLink(link.linkText);
+            })
+
+            await expect(page).toHaveURL(link.url);
+        })
+    }
+})
