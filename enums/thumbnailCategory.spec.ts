a3ecc6a307ce15ad0aa441ba86d47d1b71c16789
aksi1984 <pablo_xmen@op.pl>
2023-11-28 08:52:48 +0100

    Created the enum

---
+++ enums/thumbnailCategory.spec.ts
@@ -0,0 +1,12 @@
+enum ThumbnailCategory {
+
+    AllBlackTops = '1',
+    HighHeelShoes = '2',
+    MostWanted = '3',
+    Scarfs = '4',
+    OnSale = '5',
+    Featured = '6',
+    Trends = '7', 
+    Recent1 = '1',
+    Recent2 = '2'
+}
---
+++ enums/thumbnailType.spec.ts
@@ -0,0 +1,5 @@
+enum ThumbnailType {
+
+    Product,
+    Blog
+}
