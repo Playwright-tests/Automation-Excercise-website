95f522e84b484e5c5afeaa4bed1a21230f5a24f3
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-03 15:50:19 +0100

    Created the function

---
+++ helpers/newsletterData.spec.ts
@@ -0,0 +1,20 @@
+import { getJSONArray } from "./JSONArray.spec";
+
+export function getNewsletterData(node: string) {
+
+    let JSONArray = getJSONArray('newsletter');
+    let newsletterData: { name: string, email: string, message: string }[] = [];
+
+    for(const data of JSONArray[node]) {
+
+        let temp = {
+            name: data.name,
+            email: data.email,
+            message: data.message
+        }
+
+        newsletterData.push(temp);
+    }
+
+    return newsletterData;
+}
