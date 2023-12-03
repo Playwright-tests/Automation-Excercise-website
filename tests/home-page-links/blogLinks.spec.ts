b5d092c4b32df40053015a5befebf592c5541345
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-03 15:54:48 +0100

    Added tests

--- tests/home-page-links/blogLinks.spec.ts
+++ tests/home-page-links/blogLinks.spec.ts
@@ -2,24 +2,32 @@ import { ThumbnailCategory } from "../../enums/thumbnailCategory.spec";
 import { ThumbnailType } from "../../enums/thumbnailType.spec";
 import { test, expect } from "../../fixtures/thumbnail.spec";
 import { getLinkData } from "../../helpers/link.spec";
+import { steps } from "./steps.spec";
 
-const links = getLinkData('blogs_1');
+const links_1 = getLinkData('blogs_1');
+const links_2 = getLinkData('blogs_2');
 
 test.use({thumbnailType: ThumbnailType.Blog});
 
 test.describe('Home page links to blogs from the first section',async () => {
     
-    for(const link of links) {
+    for(const link of links_1) {
 
         test('Clicking the "' + link.linkText + '" link',async ({thumbnailGenerator, page}) => {
 
-            const thumbnail = thumbnailGenerator.createThumbnail(page, ThumbnailCategory.Recent1, link.linkText);
+            await steps(page, ThumbnailCategory.Recent1, thumbnailGenerator, link.linkText);
+            await expect(page).toHaveURL(link.url);
+        })
+    }
+})
 
-            await test.step('Click the "' + link.linkText + '" link',async () => {
-                
-                await thumbnail.clickLink();
-            })
+test.describe('Home page links to blogs from the second section',async () => {
+    
+    for(const link of links_2) {
 
+        test('Clicking the "' + link.linkText + '" link',async ({thumbnailGenerator, page}) => {
+
+            await steps(page, ThumbnailCategory.Recent2, thumbnailGenerator, link.linkText);
             await expect(page).toHaveURL(link.url);
         })
     }
