b4d93d82408d1991af72b258aecb32b4de1afee8
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-03 10:54:31 +0100

    Added tests to other categories

--- tests/home-page-links/productLinks.spec.ts
+++ tests/home-page-links/productLinks.spec.ts
@@ -1,21 +1,97 @@
 import { ThumbnailCategory } from "../../enums/thumbnailCategory.spec";
 import { test, expect } from "../../fixtures/thumbnail.spec";
 import { getLinkData } from "../../helpers/link.spec";
+import { steps } from "./steps.spec";
 
-const links = getLinkData('allBlackTopsProducts');
+const allBlackTops = getLinkData('allBlackTopsProducts');
+const highHeelShoes = getLinkData('highHeelShoesProducts');
+const mostWanted = getLinkData('mostWantedProducts');
+const scarfs = getLinkData('scarfsProducts');
+const onSale = getLinkData('onSaleProducts');
+const featured = getLinkData('featuredProducts');
+const trends = getLinkData('trendsProducts');
 
-test.describe('Home page links to products from "ALL BLACK TOPS" category',async () => {
+
+test.describe('Home page links to products from  the "ALL BLACK TOPS" category',async () => {
     
-    for(const link of links) {
+    for(const link of allBlackTops) {
 
         test('Clicking the "' + link.linkText + '" link',async ({thumbnailGenerator, page}) => {
             
-            const thumbnail = thumbnailGenerator.createThumbnail(page, ThumbnailCategory.AllBlackTops, link.linkText);
+            await steps(page, ThumbnailCategory.AllBlackTops, thumbnailGenerator, link.linkText);
+            await expect(page).toHaveURL(link.url);
+        })
+    }
+})
 
-            await test.step('Click the "' + link.linkText + '" link',async () => {
-                
-                await thumbnail.clickLink();
-            })
+test.describe('Home page links to products from the "HIGH HEEL SHOES" category',async () => {
+    
+    for(const link of highHeelShoes) {
+
+        test('Clicking the "' + link.linkText + '" link',async ({thumbnailGenerator, page}) => {
+            
+            await steps(page, ThumbnailCategory.HighHeelShoes, thumbnailGenerator, link.linkText);
+            await expect(page).toHaveURL(link.url);
+        })
+    }
+})
+
+test.describe('Home page links to products from the "MOST WANTED" category',async () => {
+    
+    for(const link of mostWanted) {
+
+        test('Clicking the "' + link.linkText + '" link',async ({thumbnailGenerator, page}) => {
+            
+            await steps(page, ThumbnailCategory.MostWanted, thumbnailGenerator, link.linkText);
+            await expect(page).toHaveURL(link.url);
+        })
+    }
+})
+
+test.describe('Home page links to products from the "SCARFS" category',async () => {
+    
+    for(const link of scarfs) {
+
+        test('Clicking the "' + link.linkText + '" link',async ({thumbnailGenerator, page}) => {
+            
+            await steps(page, ThumbnailCategory.Scarfs, thumbnailGenerator, link.linkText);
+            await expect(page).toHaveURL(link.url);
+        })
+    }
+})
+
+test.describe('Home page links to products from the "ON SALE" category',async () => {
+    
+    for(const link of onSale) {
+
+        test('Clicking the "' + link.linkText + '" link',async ({thumbnailGenerator, page}) => {
+            
+            await steps(page, ThumbnailCategory.OnSale, thumbnailGenerator, link.linkText);
+            await expect(page).toHaveURL(link.url);
+        })
+    }
+})
+
+test.describe('Home page links to products from the "FEATURED" category',async () => {
+    
+    for(const link of featured) {
+
+        test('Clicking the "' + link.linkText + '" link',async ({thumbnailGenerator, page}) => {
+            
+            await steps(page, ThumbnailCategory.Featured, thumbnailGenerator, link.linkText);
+            await expect(page).toHaveURL(link.url);
+        })
+    }
+})
+
+test.describe('Home page links to products from the "TRENDS" category',async () => {
+    
+    for(const link of trends) {
+
+        test('Clicking the "' + link.linkText + '" link',async ({thumbnailGenerator, page}) => {
+            
+            await steps(page, ThumbnailCategory.Trends, thumbnailGenerator, link.linkText);
+            await expect(page).toHaveURL(link.url);
         })
     }
 })
