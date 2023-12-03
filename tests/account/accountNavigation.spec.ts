f30dff23615dc7ab3532b4786793d680913dbdd8
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-03 18:04:32 +0100

    Created tests

---
+++ tests/account/accountNavigation.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from "../../fixtures/account.spec";
+import { getLinkData } from "../../helpers/link.spec";
+
+const links = getLinkData('accountNavigation')
+
+test.describe('Account navigation',async () => {
+    
+    for(const link of links) {
+
+        test('Clicking the "' + link.linkText + '" link',async ({accountNavigation, page}) => {
+            
+            await test.step('Click the "' + link.linkText + '" link', async () => {
+                
+                await accountNavigation.clickLink(link.linkText);
+            })
+
+            await expect(page).toHaveURL(link.url);
+        })
+    }
+})
