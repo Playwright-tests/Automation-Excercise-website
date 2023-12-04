24e3ca1d6d71f5e799c4a8735462a8112e335da6
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-04 23:16:39 +0100

    Added the second fixture

--- fixtures/account.spec.ts
+++ fixtures/account.spec.ts
@@ -1,13 +1,21 @@
 import { test as base } from "@playwright/test";
 import { AccountNavigation } from "../page-object/account/accountNavigation.spec";
 import { AccountService } from "../helpers/accountService.spec";
+import { type } from "os";
+import { AddressFormNavigation } from "../page-object/account/addressFormNavigation.spec";
+import { getLinkData } from "../data-loaders/link.spec";
 
 type AccountNavigationFixture = {
 
     accountNavigation: AccountNavigation
 }
 
-export const test = base.extend<AccountNavigationFixture>({
+type AddressFormNavigationFixture = {
+
+    addressFormNavigation: AddressFormNavigation
+}
+
+export const test = base.extend<AccountNavigationFixture & AddressFormNavigationFixture>({
 
     accountNavigation:async ({page}, use) => {
         
@@ -15,6 +23,18 @@ export const test = base.extend<AccountNavigationFixture>({
         
         await AccountService.login(page);
         await use(accountNavigation);
+    },
+
+    addressFormNavigation:async ({page}, use) => {
+        
+        const accountNavigation = new AccountNavigation(page);
+        const addressFormNavigation = new AddressFormNavigation(page);
+
+        const link = getLinkData('accountNavigation');
+
+        await AccountService.login(page);
+        await accountNavigation.clickLink(link[3].linkText);
+        await use(addressFormNavigation);
     }
 })
 
