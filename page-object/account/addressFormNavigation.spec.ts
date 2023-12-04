496501cadfb5a3cbe7c886c38d1730ccbef5f0d2
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-04 23:17:19 +0100

    Created the AddressFormNavigation class

---
+++ page-object/account/addressFormNavigation.spec.ts
@@ -0,0 +1,26 @@
+import { Locator, Page } from "@playwright/test";
+import { BasePage } from "../base/BasePage.spec";
+
+export class AddressFormNavigation extends BasePage {
+
+    readonly billingAddressFormLink: Locator;
+    readonly shippingAddressFormLink: Locator;
+
+    constructor(page: Page) {
+        
+        super(page);
+
+        this.billingAddressFormLink = page.locator('header').filter({hasText: 'Billing address'}).getByRole('link');
+        this.shippingAddressFormLink = page.locator('header').filter({hasText: 'Shipping address'}).getByRole('link');
+    }
+
+    async clickBillingAddressFormEditLink() {
+
+        await this.billingAddressFormLink.click();
+    }
+
+    async clickShippingAddressFormEditLink() {
+
+        await this.shippingAddressFormLink.click();
+    }
+}
