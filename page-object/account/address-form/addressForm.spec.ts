d0a5bbc4fa1e7667efd6c05e1ad1e0c8c100b77e
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-04 23:18:35 +0100

    Updated

---
+++ page-object/account/address-form/addressForm.spec.ts
@@ -0,0 +1,103 @@
+import { Locator, Page } from "@playwright/test";
+import { BasePage } from "../../base/BasePage.spec";
+
+export class AddressForm extends BasePage {
+
+    readonly firstNameField: Locator;
+    readonly lastNameField: Locator;
+    readonly companyField: Locator;
+    readonly addressField_1: Locator;
+    readonly addressField_2: Locator;
+    readonly townCityField: Locator;
+    readonly stateCountyField: Locator;
+    readonly regionField: Locator;
+    readonly municipalityField: Locator;
+    readonly postcodeField: Locator;
+    readonly phoneField: Locator;
+    readonly emaiField: Locator;
+    readonly saveAddressButton: Locator;
+
+    constructor(page: Page) {
+
+        super(page);
+
+        this.firstNameField = page.getByLabel('First name *');
+        this.lastNameField = page.getByLabel('Last name *');
+        this.companyField = page.getByLabel('Company name');
+        this.addressField_1 = page.getByPlaceholder('House number and street name');
+        this.addressField_2 = page.getByPlaceholder("Apartment, suite, unit etc. (')");
+        this.townCityField = page.getByLabel('Town / City *');
+        this.stateCountyField = page.getByLabel('State / County *');
+        this.regionField = page.getByLabel('Region *');
+        this.municipalityField = page.getByLabel('Municipality');
+        this.postcodeField = page.getByLabel('Postcode / ZIP *');
+        this.phoneField = page.getByLabel('Phone *');
+        this.emaiField = page.getByLabel('Email address *');
+        this.saveAddressButton = page.getByRole('button', {name: 'Save address'});
+    }
+
+    async setFirstName(firstName: string) {
+
+        await this.firstNameField.fill(firstName);
+    }
+
+    async setLastName(lastName: string) {
+
+        await this.lastNameField.fill(lastName);
+    }
+
+    async setCompany(company: string) {
+
+        await this.companyField.fill(company);
+    }
+
+    async setAddress_1(address: string) {
+
+        await this.addressField_1.fill(address);
+    }
+
+    async setAddress_2(address: string) {
+
+        await this.addressField_2.fill(address);
+    }
+
+    async setTown(town: string) {
+
+        await this.townCityField.fill(town);
+    }
+
+    async setState(state: string) {
+
+        await this.stateCountyField.fill(state);
+    }
+
+    async setRegion(region: string) {
+
+        await this.regionField.fill(region);
+    }
+
+    async setMunicipality(municipality: string) {
+
+        await this.municipalityField.fill(municipality);
+    }
+
+    async setPostcode(postcode: string) {
+
+        await this.postcodeField.fill(postcode);
+    }
+
+    async setPhone(phone: string) {
+
+        this.phoneField.fill(phone);
+    }
+
+    async setEmail(email: string) {
+
+        this.emaiField.fill(email);
+    }
+
+    async clickSaveAddressButton() {
+
+        await this.saveAddressButton.click();
+    }
+}
