60ff3d45e2143b97fc7da0b8832e892c0a363c1f
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-12 18:41:46 +0100

    Added tests

--- tests/account/address-form/additionalFields.spec.ts
+++ tests/account/address-form/additionalFields.spec.ts
@@ -6,9 +6,13 @@ const stateCountyFieldCountries = getArrayData('addressForm', 'stateCountyField'
 const stateCountyDDLCountries = getArrayData('addressForm', 'stateCountyDropdownList');
 const countyDDLCountries = getArrayData('addressForm', 'countyDropdownList');
 const stateDDLCountries = getArrayData('addressForm', 'stateDropdownList');
-const districtDropdownList = getArrayData('addressForm', 'districtDropdownList');
+const districtDDLCountries = getArrayData('addressForm', 'districtDropdownList');
 const provinceDDLCountries = getArrayData('addressForm', 'provinceDropdownList');
 const regionFieldCountries = getArrayData('addressForm', 'regionField');
+const regionDDLCountries = getArrayData('addressForm', 'regionDropdownList');
+const prefectureDropdownListCountries = getArrayData('addressForm', 'prefectureDropdownList');
+const municipalityFieldCountries = getArrayData('addressForm', 'municipalityField');
+const stateZoneDDLCountries = getArrayData('addressForm', 'stateZoneDropdownList');
 
 test.describe('Additional "State / County" field',async () => {
     
@@ -60,7 +64,7 @@ test.describe('Additional "State dropdown list"',async () => {
 
 test.describe('Additional "District dropdown list"',async () => {
     
-    for(const country of districtDropdownList) {
+    for(const country of districtDDLCountries) {
 
         test('Setting "' + country + '"',async ({addressForm}) => {
             
@@ -93,3 +97,40 @@ test.describe('Additional "Region" field',async () => {
         })
     }
 })
+
+test.describe('Additional "Region" dropdown list',async () => {
+    
+    for(const country of regionDDLCountries) {
+
+        test('Setting "' + country + '"',async ({addressForm}) => {
+            
+            await dropdownListSteps(country, addressForm);
+            expect(await addressForm.getRegionDropdownList().isVisible()).toBeTruthy();
+        })
+    }
+})
+
+test.describe('Additional "Municipality" field',async () => {
+    
+    for(const country of municipalityFieldCountries) {
+
+        test('Setting "' + country + '"',async ({addressForm}) => {
+            
+            await dropdownListSteps(country, addressForm);
+            expect(await addressForm.getMunicipalityField().isVisible()).toBeTruthy();
+        })
+    }
+})
+
+test.describe('Additional "State / Zone" dropdown list',async () => {
+    
+    for(const country of stateZoneDDLCountries) {
+
+        test('Setting "' + country + '"',async ({addressForm}) => {
+            
+            await dropdownListSteps(country, addressForm);
+            expect(await addressForm.getStateZoneDropdownList().isVisible()).toBeTruthy();
+        })
+    }
+})
+
