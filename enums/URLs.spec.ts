87d5710332e6de9d6b1f3c789b9a961cf3e83cc9
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-12 14:14:59 +0100

    Created the URLs enum

---
+++ enums/URLs.spec.ts
@@ -0,0 +1,7 @@
+export enum URLs {
+
+    HomePage = 'https://skleptest.pl/',
+    LoginPage = 'https://skleptest.pl/my-account/',
+    AddressFormNavigation = 'https://skleptest.pl/my-account/edit-address/',
+    BillingAddressForm = 'https://skleptest.pl/my-account/edit-address/billing/'
+}
