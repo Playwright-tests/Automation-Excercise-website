0ced04656b8b52a85ca407fc5d1762f247a1c125
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-12 14:18:48 +0100

    Added opening page using URLs enum

--- fixtures/login.spec.ts
+++ fixtures/login.spec.ts
@@ -1,6 +1,7 @@
 import { test as base } from "@playwright/test";
 import { LoginForm } from "../page-object/login-form/loginForm.spec";
 import { Header } from "../page-object/header/Header.spec";
+import { URLs } from "../enums/URLs.spec";
 
 type LoginFixture = {
 
@@ -11,13 +12,9 @@ export const test = base.extend<LoginFixture>({
 
     loginForm:async ({page}, use) => {
         
-        const header = new Header(page);
-
-        await header.goto();
-        await header.clickAccountButton();
-
         const loginForm = new LoginForm(page);
 
+        await loginForm.goto(URLs.LoginPage);
         await use(loginForm);
     }
 })
