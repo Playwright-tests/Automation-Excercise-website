745b65b1312f30e5bc9c241136748c031b3980a3
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-14 23:44:59 +0100

    Added tests

--- tests/account/address-form/filling.spec.ts
+++ tests/account/address-form/filling.spec.ts
@@ -1,106 +1,141 @@
 import { test, expect } from "../../../fixtures/account.spec";
 import { getAddressFormData } from "../../../data-loaders/addressFormData.spec";
-import { addressFormSteps, blankFieldsCheck } from "./steps.spec";
+import { addressFormSteps, negativeCasesCheck, positiveCasesCheck } from "./steps.spec";
 import { URLs } from "../../../enums/URLs.spec";
 import { checkAddressForm } from "../../../helpers/checkAddressForm.spec";
 
-const correctAddressFormData = getAddressFormData('correct');
-const blankFirstNameField = getAddressFormData('withoutFirstName');
-const blankLastNameField = getAddressFormData('withoutLastName');
-const blankAddressField = getAddressFormData('withoutAddress');
-const blankCityField = getAddressFormData('withoutCity');
-const blankPostcodeField = getAddressFormData('withoutPostcode');
-const blankPhoneField = getAddressFormData('withoutPhone');
-const blankEmailField = getAddressFormData('withoutEmail');
+const correct = getAddressFormData('correct')[0];
+const blankCompanyField = getAddressFormData('blankCompanyField')[0];
+const blankOptionalAddressField = getAddressFormData('blankOptionalAddressField')[0];
+const incorrectFirstName = getAddressFormData('incorrectFirstName')[0];
+const incorrectLastName = getAddressFormData('incorrectLastName')[0];
+const incorrectPostcode = getAddressFormData('incorrectPostcode')[0];
+const incorrectPhoneNumber = getAddressFormData('incorrectPhoneNumber')[0];
+const incorrectEmail = getAddressFormData('incorrectEmail');
+const blankFirstNameField = getAddressFormData('withoutFirstName')[0];
+const blankLastNameField = getAddressFormData('withoutLastName')[0];
+const blankAddressField = getAddressFormData('withoutAddress')[0];
+const blankCityField = getAddressFormData('withoutCity')[0];
+const blankPostcodeField = getAddressFormData('withoutPostcode')[0];
+const blankPhoneField = getAddressFormData('withoutPhone')[0];
+const blankEmailField = getAddressFormData('withoutEmail')[0];
 
-test.describe('Filling the billing address form',async () => {
+test.describe('Filling the billing address form', async () => {
+
+    test('Correct data', async ({ addressForm, page }) => {
+
+        await addressFormSteps(addressForm, correct);
+        await expect(page).toHaveURL(URLs.AddressFormNavigation);
+        await page.goto(URLs.BillingAddressForm);
+
+        const errors = await checkAddressForm(addressForm, correct);
+        await positiveCasesCheck(errors);
+    })
+
+    test('Blank "Company" field',async ({addressForm, page}) => {
+        
+        await addressFormSteps(addressForm, blankCompanyField);
+        await expect(page).toHaveURL(URLs.AddressFormNavigation);
+        await page.goto(URLs.BillingAddressForm);
+
+        const errors = await checkAddressForm(addressForm, blankCompanyField);
+        await positiveCasesCheck(errors);
+    })
+
+    test('Blank optional "Street address" field',async ({addressForm, page}) => {
+        
+        await addressFormSteps(addressForm, blankOptionalAddressField);
+        await expect(page).toHaveURL(URLs.AddressFormNavigation);
+        await page.goto(URLs.BillingAddressForm);
+
+        const errors = await checkAddressForm(addressForm, blankOptionalAddressField);
+        await positiveCasesCheck(errors);
+    })
+})
+
+test.describe('Incorrect data',async () => {
     
-    for(const data of correctAddressFormData) {
+    test('Incorrect first name',async ({addressForm, page}) => {
+        
+        await addressFormSteps(addressForm, incorrectFirstName);
+        await negativeCasesCheck(addressForm, incorrectFirstName, page);
+    })
 
-        test('Correct data',async ({addressForm, page}) => {
+    test('Incorrect last name',async ({addressForm, page}) => {
+        
+        await addressFormSteps(addressForm, incorrectLastName);
+        await negativeCasesCheck(addressForm, incorrectLastName, page);
+    })
+
+    test('Incorrect postcode',async ({addressForm, page}) => {
+        
+        await addressFormSteps(addressForm, incorrectPostcode);
+        await negativeCasesCheck(addressForm, incorrectPostcode, page);
+    })
+
+    test('Incorrect phone number',async ({addressForm, page}) => {
+        
+        await addressFormSteps(addressForm, incorrectPhoneNumber);
+        await negativeCasesCheck(addressForm, incorrectPhoneNumber, page);
+    })
+
+    for(const data of incorrectEmail) {
+
+        test('Entering "' + data.email + '" as the incorrect email format',async ({addressForm, page}) => {
             
             await addressFormSteps(addressForm, data);
-            await expect(page).toHaveURL(URLs.AddressFormNavigation);
-            
-            await page.goto(URLs.BillingAddressForm);
-
-            const errors = await checkAddressForm(addressForm, data);
-                
-            if(errors.length === 0) {
-
-                expect(true).toBeTruthy();
-
-            } else {
-
-                console.error('Validation errors:', errors);
-                expect(false).toBeTruthy();
-            }
+            await negativeCasesCheck(addressForm, data, page);
         })
     }
 })
 
-test.describe('Blank form fields',async () => {
-    
-    for(const data of blankFirstNameField) {
+test.describe('Blank form fields', async () => {
 
-        test('Blank "First name" field',async ({addressForm, page}) => {
-            
-            await addressFormSteps(addressForm, data);
-            await blankFieldsCheck(addressForm, data, page);
-        })
-    }
+    test('Blank "First name" field', async ({ addressForm, page }) => {
 
-    for(const data of blankLastNameField) {
+        await addressFormSteps(addressForm, blankFirstNameField);
+        await negativeCasesCheck(addressForm, blankFirstNameField, page);
+    })
 
-        test('Blank "Last name" field',async ({addressForm, page}) => {
-            
-            await addressFormSteps(addressForm, data);
-            await blankFieldsCheck(addressForm, data, page);
-        })
-    }
 
-    for(const data of blankAddressField) {
+    test('Blank "Last name" field', async ({ addressForm, page }) => {
 
-        test('Blank required "Street address" field',async ({addressForm, page}) => {
-            
-            await addressFormSteps(addressForm, data);
-            await blankFieldsCheck(addressForm, data, page);
-        })
-    }
+        await addressFormSteps(addressForm, blankLastNameField);
+        await negativeCasesCheck(addressForm, blankLastNameField, page);
+    })
 
-    for(const data of blankCityField) {
 
-        test('Blank "City" field',async ({addressForm, page}) => {
-            
-            await addressFormSteps(addressForm, data);
-            await blankFieldsCheck(addressForm, data, page);
-        })
-    }
+    test('Blank required "Street address" field', async ({ addressForm, page }) => {
 
-    for(const data of blankPostcodeField) {
+        await addressFormSteps(addressForm, blankAddressField);
+        await negativeCasesCheck(addressForm, blankAddressField, page);
+    })
 
-        test('Blank "Postcode" field',async ({addressForm, page}) => {
-            
-            await addressFormSteps(addressForm, data);
-            await blankFieldsCheck(addressForm, data, page);
-        })
-    }
 
-    for(const data of blankPhoneField) {
+    test('Blank "City" field', async ({ addressForm, page }) => {
 
-        test('Blank "Phone" field',async ({addressForm, page}) => {
-            
-            await addressFormSteps(addressForm, data);
-            await blankFieldsCheck(addressForm, data, page);
-        })
-    }
+        await addressFormSteps(addressForm, blankCityField);
+        await negativeCasesCheck(addressForm, blankCityField, page);
+    })
 
-    for(const data of blankEmailField) {
 
-        test('Blank "Email" field',async ({addressForm, page}) => {
-            
-            await addressFormSteps(addressForm, data);
-            await blankFieldsCheck(addressForm, data, page);
-        })
-    }
+    test('Blank "Postcode" field', async ({ addressForm, page }) => {
+
+        await addressFormSteps(addressForm, blankPostcodeField);
+        await negativeCasesCheck(addressForm, blankPostcodeField, page);
+    })
+
+
+    test('Blank "Phone" field', async ({ addressForm, page }) => {
+
+        await addressFormSteps(addressForm, blankPhoneField);
+        await negativeCasesCheck(addressForm, blankPhoneField, page);
+    })
+
+
+    test('Blank "Email" field', async ({ addressForm, page }) => {
+
+        await addressFormSteps(addressForm, blankEmailField);
+        await negativeCasesCheck(addressForm, blankEmailField, page);
+    })
 })
