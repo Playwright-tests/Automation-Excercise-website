dba37e909d5370aec80d7a1b5307b0be6973b7fc
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-03 17:01:28 +0100

    Added fields and methods

--- page-object/shopping-cart/shoppingCart.spec.ts
+++ page-object/shopping-cart/shoppingCart.spec.ts
@@ -1,8 +1,14 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 import { BasePage } from "../base/BasePage.spec";
 import { Row } from "./row.spec";
 
 export class ShoppingCart extends BasePage {
+
+    readonly messageSelector: string;
+
+    readonly cartEmpty: Locator;
+    readonly updateButton: Locator;
+    readonly message: Locator;
 
     private row: Row;
 
@@ -10,7 +16,38 @@ export class ShoppingCart extends BasePage {
 
         super(page);
 
+        this.messageSelector = '.woocommerce-message';
+
+        this.cartEmpty = page.getByText('Your cart is currently empty.');
+        this.updateButton = page.getByRole('button', {name: 'Update cart'});
+        this.message = page.locator(this.messageSelector);
+
         this.row = new Row(page);
+    }
+
+    getCartEmptyLocator() {
+
+        return this.cartEmpty;
+    }
+
+    async clickUpdateButton() {
+
+        await this.updateButton.click();
+    }
+
+    async getMessageText() {
+
+        return await this.message.textContent();
+    }
+
+    getMessageLocator() {
+
+        return this.message;
+    }
+
+    getMessageSelector() {
+
+        return this.messageSelector;
     }
 
     getRow() {
