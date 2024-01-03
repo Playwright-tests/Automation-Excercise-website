82b9ee3686f48b9cf3c25a1ded4493430fe0cae7
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-03 16:58:08 +0100

    Created function

---
+++ helpers/checkShoppingCart.spec.ts
@@ -0,0 +1,17 @@
+import { ShoppingCart } from "../page-object/shopping-cart/shoppingCart.spec";
+
+export async function checkShoppingCartState(shoppingCart: ShoppingCart, oldCount: number) {
+    
+    const errors: string[] = [];
+    const expectedCount = oldCount - 1;
+
+    if(!await shoppingCart.getCartEmptyLocator().isVisible()) {
+        errors.push('The message about empty shopping cart is not displayed');
+    }
+
+    if(await shoppingCart.getRow().getCount() > oldCount) {
+        errors.push('Product has not been removed from the shopping cart- expected: "' + expectedCount + '", received: "' + await shoppingCart.getRow().getCount());
+    }
+
+    return errors;
+}
