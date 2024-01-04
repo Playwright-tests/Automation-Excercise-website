8603a4f2335d2ab7573aee854ec05241282bf84c
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-04 22:41:25 +0100

    Created the function

---
+++ helpers/addressFormFiller.spec.ts
@@ -0,0 +1,17 @@
+import { Page } from "@playwright/test";
+import { AddressForm } from "../page-object/account/address-form/addressForm.spec";
+
+export async function fillAddressForm(page: Page, data: any) {
+    
+    const addressForm = new AddressForm(page);
+
+    await addressForm.setFirstName(data.firstName);
+    await addressForm.setLastName(data.lastName);
+    await addressForm.setCompany(data.company);
+    await addressForm.setAddress_1(data.address1);
+    await addressForm.setAddress_2(data.address2);
+    await addressForm.setPostcode(data.postcode);
+    await addressForm.setTown(data.city);
+    await addressForm.setPhone(data.phone);
+    await addressForm.setEmail(data.email);
+}
