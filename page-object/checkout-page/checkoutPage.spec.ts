66ef242995c2a397703b72f98c547a5bab7516df
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-04 22:42:13 +0100

    Added field and method

--- page-object/checkout-page/checkoutPage.spec.ts
+++ page-object/checkout-page/checkoutPage.spec.ts
@@ -1,20 +1,33 @@
 import { Page } from "@playwright/test";
 import { BasePage } from "../base/BasePage.spec";
 import { DifferentAddressSection } from "./differentAddressSection.spec";
+import { PaymentMethodsSection } from "./paymentMethodsSection.spec";
 
 export class CheckoutPage extends BasePage {
 
     private differentAddressSection: DifferentAddressSection;
+    private paymentMethodsScetion: PaymentMethodsSection;
 
     constructor(page: Page) {
 
         super(page);
 
         this.differentAddressSection = new DifferentAddressSection(page);
+        this.paymentMethodsScetion = new PaymentMethodsSection(page);
+    }
+
+    async clickPlaceOrderButton(buttonName: string) {
+
+        await (await this.getPage()).getByRole('button', {name: buttonName}).click();
     }
 
     getDifferentAddressSection() {
 
         return this.differentAddressSection;
+    }
+
+    getPaymentMethodsSection() {
+
+        return this.paymentMethodsScetion;
     }
 }
