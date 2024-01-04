e3f2033d8cf00136bebe60143307f2213ae1a539
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-04 09:01:26 +0100

    Added locator and method

--- page-object/shopping-cart/shoppingCart.spec.ts
+++ page-object/shopping-cart/shoppingCart.spec.ts
@@ -9,6 +9,7 @@ export class ShoppingCart extends BasePage {
     readonly cartEmpty: Locator;
     readonly updateButton: Locator;
     readonly message: Locator;
+    readonly checkoutButton: Locator;
 
     private row: Row;
 
@@ -21,6 +22,7 @@ export class ShoppingCart extends BasePage {
         this.cartEmpty = page.getByText('Your cart is currently empty.');
         this.updateButton = page.getByRole('button', {name: 'Update cart'});
         this.message = page.locator(this.messageSelector);
+        this.checkoutButton = page.getByRole('link', {name: 'Proceed to checkout'});
 
         this.row = new Row(page);
     }
@@ -33,6 +35,11 @@ export class ShoppingCart extends BasePage {
     async clickUpdateButton() {
 
         await this.updateButton.click();
+    }
+
+    async clickCheckoutButton() {
+
+        await this.checkoutButton.click();
     }
 
     async getMessageText() {
