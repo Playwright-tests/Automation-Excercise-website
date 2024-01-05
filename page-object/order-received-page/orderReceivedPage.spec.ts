7fda1fb8dd47d58bfbe6054d255586271954284c
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-05 11:41:39 +0100

    Created the OrderReceivedPage class

---
+++ page-object/order-received-page/orderReceivedPage.spec.ts
@@ -0,0 +1,24 @@
+import { Locator, Page } from "@playwright/test";
+import { BasePage } from "../base/BasePage.spec";
+
+export class OrderReceivedPage extends BasePage {
+
+    readonly orderDetailsTitleSelector: string;
+
+    constructor(page: Page) {
+
+        super(page);
+
+        this.orderDetailsTitleSelector = '.woocommerce-order-details__title';
+    }
+
+    async getPaymentMethodLocator(methodName: string) {
+
+        return (await this.getPage()).locator('strong').filter({hasText: methodName});
+    }
+
+    getOrderDetailsTitleSelector() {
+
+        return this.orderDetailsTitleSelector;
+    }
+}
