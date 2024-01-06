75f36f2637931c5b23ac74878c2b471c3ee92346
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-06 17:43:45 +0100

    Created the expect

---
+++ expect/toHaveHiddenSelector.ts
@@ -0,0 +1,30 @@
+import { expect as baseExpect, Page } from "@playwright/test";
+
+export const expect = baseExpect.extend({
+
+    async toHaveHiddenSelector(page: Page, selector: string) {
+
+        const assertionName = 'toBeHidden';
+        let pass: boolean;
+
+        try {
+
+            await page.waitForSelector(selector, { timeout: 3000, state: 'hidden'});
+            pass = true;
+
+        } catch (e: any) {
+
+            pass = false;
+        }
+
+        const message = pass ? () => 'The selector "' + selector + '" is hidden' :
+                               () => 'The selector "' + selector + '" is not hidden';
+
+        return {
+
+            name: assertionName,
+            pass,
+            message
+        }
+    },
+})
