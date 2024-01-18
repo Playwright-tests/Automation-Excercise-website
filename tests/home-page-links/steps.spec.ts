a4f2db8cf2025eee97fc3d73dccf60565496f40a
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-18 11:24:44 +0100

    Updated import

--- tests/home-page-links/steps.spec.ts
+++ tests/home-page-links/steps.spec.ts
@@ -1,10 +1,10 @@
 import { Page, test } from "@playwright/test";
 import { ThumbnailCategory } from "../../enums/thumbnailCategory";
-import { ThumbnailGenerator } from "../../providers/thumbnails/thumbnailGenerator";
+import { ThumbnailFactory } from "../../providers/thumbnails/thumbnailFactory";
 
-export async function steps(page: Page, category: ThumbnailCategory, generator: ThumbnailGenerator, linkText: string) {
+export async function steps(page: Page, category: ThumbnailCategory, factory: ThumbnailFactory, linkText: string) {
     
-    const thumbnail = generator.createThumbnail(page, category, linkText);
+    const thumbnail = factory.createThumbnail(page, category, linkText);
 
     await test.step('Click the "' + linkText + '"',async () => {
         
