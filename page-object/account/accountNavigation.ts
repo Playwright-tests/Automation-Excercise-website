f850c0f5d03d888016d9c68b7f6e76408a5f4ca9
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-20 08:06:37 +0100

    Added field and method

--- page-object/account/accountNavigation.ts
+++ page-object/account/accountNavigation.ts
@@ -3,13 +3,22 @@ import { BasePage } from "../base/BasePage";
 
 export class AccountNavigation extends BasePage {
 
+    readonly contentsSelector: string;
+
     constructor(page: Page) {
 
         super(page);
+
+        this.contentsSelector = '.woocommerce-MyAccount-navigation'
     }
 
     async clickLink(linkText: string) {
 
         await (await this.getPage()).getByRole('link', {name: linkText, exact: true}).click();
+    }
+
+    getContentsSelector() {
+
+        return this.contentsSelector;
     }
 }
