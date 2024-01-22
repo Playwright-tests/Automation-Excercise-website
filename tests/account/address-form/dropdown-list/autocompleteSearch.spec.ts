dc6d15e6d3a54bca6129cc101dbdfdfaaa948fb8
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-22 18:11:49 +0100

    Created test

---
+++ tests/account/address-form/dropdown-list/autocompleteSearch.spec.ts
@@ -0,0 +1,27 @@
+import { getArrayData } from "../../../../data-loaders/arrayData";
+import { test, expect } from "../../../../fixtures/account";
+import { toLowerCase } from "../../../../utils/toLowerCase";
+import { fillDropdownList } from "../steps.spec";
+
+const incompleteCountryNames = getArrayData('countries', 'incompleteCountryNames');
+
+
+test.describe('Autocomplete search',async () => {
+    
+    for(const incompleteCountryName of incompleteCountryNames) {
+
+        test('Typiyng "'+ incompleteCountryName +'"',async ({addressForm}) => {
+        
+            await fillDropdownList(incompleteCountryName, addressForm);
+            
+            const resultsLocator = addressForm.getDropdownList().getResultsLocator();
+            const countries = resultsLocator.locator(addressForm.getDropdownList().getOptionResultSelector()).all();
+
+            for(const country of await countries) {
+    
+                const received = await toLowerCase(await country.textContent());
+                expect.soft(received).toContain(incompleteCountryName.toLowerCase());
+            }
+        })
+    }
+})
