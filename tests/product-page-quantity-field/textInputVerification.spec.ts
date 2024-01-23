705665c88c10ae59fc8ebc26e78a9467a2f0594d
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-23 18:28:12 +0100

    Created test

---
+++ tests/product-page-quantity-field/textInputVerification.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from "../../fixtures/productPage";
+
+test.describe('Text input verification',async () => {
+    
+    const quantity: string = '50';
+
+    test('Filling the quantity field',async ({productPage}) => {
+        
+        await test.step('Enter the "' + quantity + '"',async () => {
+            
+            await productPage.getQuantityField().setQuantity(quantity);
+        })
+
+        expect(await productPage.getQuantityField().getQuantity()).toEqual(quantity);
+    })
+})
