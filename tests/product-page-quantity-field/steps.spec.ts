0517797bb78fbd26358c1ece737a26c60935a29c
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-23 07:01:10 +0100

    Moved steps to this file

---
+++ tests/product-page-quantity-field/steps.spec.ts
@@ -0,0 +1,15 @@
+import { test } from "@playwright/test";
+import { ProductPage } from "../../page-object/product-page/productPage";
+
+export async function steps(productPage: ProductPage, value: string) {
+    
+    await test.step('Enter "' + value + '"in the quantity field',async () => {
+        
+        await productPage.getQuantityField().setQuantity(value);
+    })
+
+    await test.step('Click the "Add to cart" button',async () => {
+        
+        await productPage.clickAddToCartButton();
+    })
+}
