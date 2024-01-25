373f164087d57653ab02ffe6eb6fe9244b506edc
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-25 09:45:47 +0100

    Created

---
+++ tests/shopping-cart/steps.spec.ts
@@ -0,0 +1,15 @@
+import { test } from "@playwright/test";
+import { ShoppingCart } from "../../page-object/shopping-cart/shoppingCart";
+
+export async function steps(shoppingCart: ShoppingCart, index: number, quantity: string) {
+
+    await test.step('Enter "' + quantity + '"in the quantity field',async () => {
+        
+        await (await shoppingCart.getRow(index).getQuantityField()).setQuantity(quantity);
+    })
+
+    await test.step('Click the "Update cart" button',async () => {
+        
+        await shoppingCart.clickUpdateButton();
+    })
+}
