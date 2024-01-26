cfb5c96bd7091b8b884334be02b7bcf51d3cd0e9
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-26 23:22:45 +0100

    Created

---
+++ mockaroo-testdata-provider/mockarooTestDataProvider.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { MockarooTestdata } from "../enums/mockarooTestData";
+
+export class MockaroTesdataProvider {
+
+    static readonly url: string = 'https://my.api.mockaroo.com/';
+    static readonly format = '.json';
+    static readonly apiKey = '?key=df87e700'; 
+
+    private static getUrl(mockarooTestData: MockarooTestdata) {
+
+        return this.url + mockarooTestData + this.format + this.apiKey;
+    }
+
+    static async fetchData(mockarooTestdata: MockarooTestdata) {
+
+        console.log('API: ' + this.getUrl(mockarooTestdata));
+
+        try {
+            const response = await axios.get(this.getUrl(mockarooTestdata));
+            return response.data;
+        } catch {
+            console.error('Could not fetch data from url: ' + this.getUrl(mockarooTestdata));    
+        }
+    }
+}
