5c9e539b444606be17b4b483a43f4c5b2f076546
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-27 14:42:07 +0100

    Added using the LoginTestdataLoader

--- helpers/accountService.ts
+++ helpers/accountService.ts
@@ -1,20 +1,19 @@
 import { Page } from "@playwright/test";
 import { LoginForm } from "../page-object/login-form/loginForm";
-import { getCredentials } from "../data-loaders/credentials";
 import { URLs } from "../enums/URLs";
+import { LoginTestdataLoader } from "../data-loaders/mockarooDataLoaders";
 
 export class AccountService {
 
     static async login(page: Page) {
         
-        let correctCredentials = getCredentials('correctCredentials');
-        let first = correctCredentials[0];
+        LoginTestdataLoader.init();
 
         const loginForm = new LoginForm(page);
 
         await loginForm.goto(URLs.LoginPage);
-        await loginForm.setUsername(first.usernameOrEmail);
-        await loginForm.setPassword(first.password);
+        await loginForm.setUsername(await LoginTestdataLoader.correct.username);
+        await loginForm.setPassword(await LoginTestdataLoader.correct.password);
         await loginForm.clickLoginButton();
     }
 }
