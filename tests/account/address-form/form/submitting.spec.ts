4d98a6db1d501bc57770956457b7350dcc216cd0
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-27 09:02:22 +0100

    Added the test.beforeAll

--- tests/account/address-form/form/submitting.spec.ts
+++ tests/account/address-form/form/submitting.spec.ts
@@ -1,22 +1,25 @@
-import { getAddressFormData } from "../../../../data-loaders/addressFormData";
+import { AddressFormTestdataLoader } from "../../../../data-loaders/mockarooDataLoaders";
 import { URLs } from "../../../../enums/URLs";
 import { test, expect } from "../../../../fixtures/account";
 import { fillAddressForm, fillAddressFormAndAcceptChanges } from "../steps.spec";
 
-const correct = getAddressFormData('correct')[0];
-
 
 test.describe('Submitting',async () => {
     
+    test.beforeAll(async () => {
+        
+        AddressFormTestdataLoader.init();
+    })
+
     test('Clicking the "Save address" button',async ({addressForm}) => {
         
-        await fillAddressFormAndAcceptChanges(addressForm, correct);
+        await fillAddressFormAndAcceptChanges(addressForm, AddressFormTestdataLoader.correct);
         await expect(await addressForm.getPage()).toHaveURL(URLs.AddressFormNavigation);
     })
 
     test('Pressing ENTER',async ({addressForm}) => {
         
-        await fillAddressForm(addressForm, correct);
+        await fillAddressForm(addressForm, AddressFormTestdataLoader.correct);
 
         await test.step('Press ENTER',async () => {
             
