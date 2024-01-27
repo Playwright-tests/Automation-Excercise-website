ad31ba75867d71847cf640ce6e1d2213701a4b2a
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-27 22:41:24 +0100

    Removed loops

--- tests/account/address-form/form/additionalFields.spec.ts
+++ tests/account/address-form/form/additionalFields.spec.ts
@@ -1,161 +1,91 @@
-import { getArrayData } from "../../../../data-loaders/arrayData";
 import { test, expect } from "../../../../fixtures/account";
 import { checkLabel } from "./assertions.spec";
 import { fillDropdownListAndAcceptChanges } from "../steps.spec";
+import { AddressFormAdditionalFieldsTestdataLoader } from "../../../../data-loaders/dataLoaders";
 
-const stateCountyField = getArrayData('addressForm', 'stateCountyField');
-const stateCountyDropdownList = getArrayData('addressForm', 'stateCountyDropdownList');
-const countyDropdownList = getArrayData('addressForm', 'countyDropdownList');
-const stateDropdownList = getArrayData('addressForm', 'stateDropdownList');
-const districtDropdownList = getArrayData('addressForm', 'districtDropdownList');
-const provinceDropdownList = getArrayData('addressForm', 'provinceDropdownList');
-const regionField = getArrayData('addressForm', 'regionField');
-const regionDropdownList = getArrayData('addressForm', 'regionDropdownList');
-const prefectureDropdownList = getArrayData('addressForm', 'prefectureDropdownList');
-const municipalityField = getArrayData('addressForm', 'municipalityField');
-const stateZoneDropdownList = getArrayData('addressForm', 'stateZoneDropdownList');
 
 test.describe('Additional "State / County" field',async () => {
     
-    for(const country of stateCountyField) {
+    test.beforeAll(async () => {
+        
+        await AddressFormAdditionalFieldsTestdataLoader.init();
+    })
 
-        test('Setting "' + country + '"',async ({addressForm}) => {
+    test('Additional State county field',async ({addressForm}) => {
             
-            await fillDropdownListAndAcceptChanges(country, addressForm);
+        await fillDropdownListAndAcceptChanges(await AddressFormAdditionalFieldsTestdataLoader.stateCountyField.country, addressForm);
             
-            expect(await addressForm.getAdditionalFieldLocator().isVisible()).toBeTruthy();
-            await checkLabel(addressForm, "State / County");
-        })
-    }
+        expect(await addressForm.getAdditionalFieldLocator().isVisible()).toBeTruthy();
+        await checkLabel(addressForm, "State / County");
+    })
+
+    test('Additional "State / County dropdown list"',async ({addressForm}) => {
+            
+        await fillDropdownListAndAcceptChanges(await AddressFormAdditionalFieldsTestdataLoader.stateCountyDropdownList.country, addressForm);
+        expect(await addressForm.getAdditionalDropdownListLocator().isVisible()).toBeTruthy();
+        await checkLabel(addressForm, "State / County *");
+    })
+
+    test('Additional "County" dropdown list',async ({addressForm}) => {
+            
+        await fillDropdownListAndAcceptChanges(await AddressFormAdditionalFieldsTestdataLoader.countyDropdownList.country, addressForm);
+        expect(await addressForm.getAdditionalDropdownListLocator().isVisible()).toBeTruthy();
+        await checkLabel(addressForm, 'County *');
+    })
+
+    test('Additional "State dropdown list"',async ({addressForm}) => {
+            
+        await fillDropdownListAndAcceptChanges(await AddressFormAdditionalFieldsTestdataLoader.stateDropdownList.country, addressForm);
+        expect(await addressForm.getAdditionalDropdownListLocator().isVisible()).toBeTruthy();
+        await checkLabel(addressForm, "State *");
+    })
+
+    test('Additional "District dropdown list"',async ({addressForm}) => {
+            
+        await fillDropdownListAndAcceptChanges(await AddressFormAdditionalFieldsTestdataLoader.districtDropdownList.country, addressForm);
+        expect(await addressForm.getAdditionalDropdownListLocator().isVisible()).toBeTruthy();
+        await checkLabel(addressForm, 'District *');
+    })
+
+    test('Additional "Province dropdown list"',async ({addressForm}) => {
+            
+        await fillDropdownListAndAcceptChanges(await AddressFormAdditionalFieldsTestdataLoader.provinceDropdownList.country, addressForm);
+        expect(await addressForm.getAdditionalDropdownListLocator().isVisible()).toBeTruthy();
+        await checkLabel(addressForm, 'Province *');
+    })
+
+    test('Additional "Region" field',async ({addressForm}) => {
+            
+        await fillDropdownListAndAcceptChanges(await AddressFormAdditionalFieldsTestdataLoader.regionField.country, addressForm);
+        expect(await addressForm.getAdditionalFieldLocator().isVisible()).toBeTruthy();
+        await checkLabel(addressForm, 'Region *');
+    })
+
+    test('Additional "Region" dropdown list',async ({addressForm}) => {
+            
+        await fillDropdownListAndAcceptChanges(await AddressFormAdditionalFieldsTestdataLoader.regionDropdownList.country, addressForm);
+        expect(await addressForm.getAdditionalDropdownListLocator().isVisible()).toBeTruthy();
+        await checkLabel(addressForm, 'Region')
+    })
+
+    test('Additional "Prefecture" dropdown list',async ({addressForm}) => {
+            
+        await fillDropdownListAndAcceptChanges(await AddressFormAdditionalFieldsTestdataLoader.prefectureDropdownList.country, addressForm);
+        expect(await addressForm.getAdditionalDropdownListLocator().isVisible()).toBeTruthy();
+        await checkLabel(addressForm, 'Prefecture *');
+    })
+
+    test('Additional "Municipality" field',async ({addressForm}) => {
+            
+        await fillDropdownListAndAcceptChanges(await AddressFormAdditionalFieldsTestdataLoader.municipalityField.country, addressForm);
+        expect(await addressForm.getAdditionalFieldLocator().isVisible()).toBeTruthy();
+        await checkLabel(addressForm, 'Municipality');
+    })
+
+    test('Additional "State / Zone" dropdown list',async ({addressForm}) => {
+            
+        await fillDropdownListAndAcceptChanges(await AddressFormAdditionalFieldsTestdataLoader.stateZoneDropdownList.country, addressForm);
+        expect(await addressForm.getAdditionalDropdownListLocator().isVisible()).toBeTruthy();
+        await checkLabel(addressForm, 'State / Zone *');
+    })
 })
-
-test.describe('Additional "State / County dropdown list"',async () => {
-    
-    for(const country of stateCountyDropdownList) {
-
-        test('Setting "' + country + '"',async ({addressForm}) => {
-            
-            await fillDropdownListAndAcceptChanges(country, addressForm);
-            expect(await addressForm.getAdditionalDropdownListLocator().isVisible()).toBeTruthy();
-            await checkLabel(addressForm, "State / County *");
-        })
-    }
-})
-
-test.describe('Additional "County" dropdown list',async () => {
-    
-    for(const country of countyDropdownList) {
-
-        test('Setting "' + country + '"',async ({addressForm}) => {
-            
-            await fillDropdownListAndAcceptChanges(country, addressForm);
-            expect(await addressForm.getAdditionalDropdownListLocator().isVisible()).toBeTruthy();
-            await checkLabel(addressForm, 'County *');
-        })
-    }
-})
-
-test.describe('Additional "State dropdown list"',async () => {
-    
-    for(const country of stateDropdownList) {
-
-        test('Setting "' + country + '"',async ({addressForm}) => {
-            
-            await fillDropdownListAndAcceptChanges(country, addressForm);
-            expect(await addressForm.getAdditionalDropdownListLocator().isVisible()).toBeTruthy();
-            await checkLabel(addressForm, "State *");
-        })
-    }
-})
-
-test.describe('Additional "District dropdown list"',async () => {
-    
-    for(const country of districtDropdownList) {
-
-        test('Setting "' + country + '"',async ({addressForm}) => {
-            
-            await fillDropdownListAndAcceptChanges(country, addressForm);
-            expect(await addressForm.getAdditionalDropdownListLocator().isVisible()).toBeTruthy();
-            await checkLabel(addressForm, 'District *');
-        })
-    }
-})
-
-test.describe('Additional "Province dropdown list"',async () => {
-    
-    for(const country of provinceDropdownList) {
-
-        test('Setting "' + country + '"',async ({addressForm}) => {
-            
-            await fillDropdownListAndAcceptChanges(country, addressForm);
-            expect(await addressForm.getAdditionalDropdownListLocator().isVisible()).toBeTruthy();
-            await checkLabel(addressForm, 'Province *');
-        })
-    }
-})
-
-test.describe('Additional "Region" field',async () => {
-    
-    for(const country of regionField) {
-
-        test('Setting "' + country + '"',async ({addressForm}) => {
-            
-            await fillDropdownListAndAcceptChanges(country, addressForm);
-            expect(await addressForm.getAdditionalFieldLocator().isVisible()).toBeTruthy();
-            await checkLabel(addressForm, 'Region *');
-        })
-    }
-})
-
-test.describe('Additional "Region" dropdown list',async () => {
-    
-    for(const country of regionDropdownList) {
-
-        test('Setting "' + country + '"',async ({addressForm}) => {
-            
-            await fillDropdownListAndAcceptChanges(country, addressForm);
-            expect(await addressForm.getAdditionalDropdownListLocator().isVisible()).toBeTruthy();
-            await checkLabel(addressForm, 'Region')
-        })
-    }
-})
-
-test.describe('Additional "Prefecture" dropdown list',async () => {
-    
-    for(const country of prefectureDropdownList) {
-
-        test('Setting "' + country + '"',async ({addressForm}) => {
-            
-            await fillDropdownListAndAcceptChanges(country, addressForm);
-            expect(await addressForm.getAdditionalDropdownListLocator().isVisible()).toBeTruthy();
-            await checkLabel(addressForm, 'Prefecture *');
-        })
-    }
-})
-
-test.describe('Additional "Municipality" field',async () => {
-    
-    for(const country of municipalityField) {
-
-        test('Setting "' + country + '"',async ({addressForm}) => {
-            
-            await fillDropdownListAndAcceptChanges(country, addressForm);
-            expect(await addressForm.getAdditionalFieldLocator().isVisible()).toBeTruthy();
-            await checkLabel(addressForm, 'Municipality');
-        })
-    }
-})
-
-test.describe('Additional "State / Zone" dropdown list',async () => {
-    
-    for(const country of stateZoneDropdownList) {
-
-        test('Setting "' + country + '"',async ({addressForm}) => {
-            
-            await fillDropdownListAndAcceptChanges(country, addressForm);
-            expect(await addressForm.getAdditionalDropdownListLocator().isVisible()).toBeTruthy();
-            await checkLabel(addressForm, 'State / Zone *');
-        })
-    }
-})
-
