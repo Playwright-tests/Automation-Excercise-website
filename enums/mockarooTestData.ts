2ca72a24ff8d6f1492e967f26b7b4ba775ce654e
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-27 21:29:05 +0100

    Updated

--- enums/mockarooTestData.ts
+++ enums/mockarooTestData.ts
@@ -1,27 +1,48 @@
 export enum LoginTestdata {
 
-    Correct = 'login_correct',
-    IncorrectUsername = 'login_incorrect_username',
-    IncorrectPassword = 'login_incorrect_password',
-    BlankUsernameField = 'login_blank_username_field',
-    BlankPasswordField = 'login_blank_password_field'
+    Correct = 'LoginCorrect',
+    IncorrectUsername = 'LoginIncorrectUsername',
+    IncorrectPassword = 'LoginIncorrectPassword',
+    BlankUsernameField = 'LoginBlankUsernameField',
+    BlankPasswordField = 'LoginBlankPasswordField'
 }
 
 export enum AddressFormTestdata {
 
-    Correct = 'address_form_correct_data',
-    IncorrectFirstName = 'address_form_incorrect_first_name',
-    IncorrectLastName = 'address_form_incorrect_last_name',
-    IncorrectPostcode = 'address_form_incorrect_postcode',
-    IncorrectPhone = 'address_form_incorrect_phone',
-    IncorrectEmailFormat = 'address_form_incorrect_email_format',
-    BlankCompanyField = 'address_form_blank_company_field',
-    BlankSecondAddressLineField = 'address_form_blank_second_address_line_field',
-    BlankFirstNameField = 'address_form_blank_first_name_field',
-    BlankLastNameField = 'address_form_blank_last_name_field',
-    BlankAddressField = 'address_form_blank_address_field',
-    BlankPostcodeField = 'address_form_blank_postcode_field',
-    BlankCityField = 'address_form_blank_city_field',
-    BlankPhoneField = 'address_form_blank_phone_field',
-    BlankEmailField = 'address_form_blank_email_field',
+    Correct = 'AddressFormCorrectData',
+    IncorrectFirstName = 'AddressFormIncorrectFirstName',
+    IncorrectLastName = 'AddressFormIncorrectLastName',
+    IncorrectPostcode = 'AddressFormIncorrectPostcode',
+    IncorrectPhone = 'AddressFormIncorrectPhone',
+    IncorrectEmailFormat = 'AddressFormIncorrectEmailFormat',
+    BlankCompanyField = 'AddressFormBlankCompanyField',
+    BlankSecondAddressLineField = 'AddressFormBlankSecondAddressLineField',
+    BlankFirstNameField = 'AddressFormBlankFirstNameField',
+    BlankLastNameField = 'AddressFormBlankLastNameField',
+    BlankAddressField = 'AddressFormBlankAddressField',
+    BlankPostcodeField = 'AddressFormBlankPostcodeField',
+    BlankCityField = 'AddressFormBlankCityField',
+    BlankPhoneField = 'AddressFormBlankPhoneField',
+    BlankEmailField = 'AddressFormBlankEmailField',
+}
+
+export enum DropdownListCountryNameTestdata {
+
+    Correct = 'DropdownListCorrectCountryName',
+    Incorrect = 'DropdownListIncorrectCountryName',
+}
+
+export enum DropdownListAdditionalFieldsTestdata {
+
+    CountyDropdownList = 'CountyDropdownList',
+    DistrictDropdownList = 'DistrictDropdownList',
+    MunicipalityField = 'MunicipalityField',
+    PrefectureDropdownList = 'PrefectureDropdownList',
+    ProvinceDropdownList = 'ProvinceDropdownList',
+    RegionDropdownList = 'RegionDropdownList',
+    RegionField = 'RegionField',
+    StateCountyDropdownList = 'StateCountyDropdownList',
+    StateCountyField = 'StateCountyField',
+    StateDropdownList = 'StateDropdownList',
+    StateZoneDropdownList = 'StateZoneDropdownList'
 }
