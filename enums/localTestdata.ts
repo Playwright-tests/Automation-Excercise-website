d0477dc920d31d7d6bff7ee3675a176955c85490
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-27 23:57:38 +0100

    Created the enum

---
+++ enums/localTestdata.ts
@@ -0,0 +1,10 @@
+export enum Products {
+
+    AllBlackTops = 'LinksAllBlackTops',
+    Featured = 'LinksFeatured',
+    HighHeelShoes = 'LinksHighHeelShoes',
+    MostWanted = 'LinksMostWanted',
+    OnSale = 'LinksOnSale',
+    Scarfs = 'LinksScarfs',
+    Trends = 'LinksTrends'
+}
