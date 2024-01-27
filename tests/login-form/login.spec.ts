7bd31fbd8a8ad7432ff5f5f99b4015bf04c65368
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-27 14:43:45 +0100

    Added using the LoginTestdataLoader

--- tests/login-form/login.spec.ts
+++ tests/login-form/login.spec.ts
@@ -2,18 +2,18 @@ import { expect, test } from "../../fixtures/login";
 import { expect as hdExpect } from "../../expect/toHaveHiddenSelector";
 import { expect as nhdExpect } from "../../expect/tohaveNotHiddenSelector";
 import { steps } from "./steps.spec";
-import { getCredentials } from "../../data-loaders/credentials";
 import { AccountNavigation } from "../../page-object/account/accountNavigation";
+import { LoginTestdataLoader } from "../../data-loaders/mockarooDataLoaders";
 
-const correct = getCredentials('correctCredentials')[0];
-const incorrectUsernameOrEmail = getCredentials('incorrectUsernameOrEmail')[0];
-const blankUsernameOrEmailField = getCredentials('blankUsernameOrEmailField')[0];
-const incorrectPassword = getCredentials('incorrectPassword');
-const blankPasswordField = getCredentials('blankPasswordField')[0];
 
 test.describe('Login',async () => {
 
     let accountNavigation: AccountNavigation;
+
+    test.beforeAll(async () => {
+        
+        LoginTestdataLoader.init();
+    })
 
     test.beforeEach(async ({page}) => {
         
@@ -22,37 +22,41 @@ test.describe('Login',async () => {
     
     test('Correct credentials',async ({loginForm}) => {
              
-        await steps(loginForm, correct);
+        await steps(loginForm, await LoginTestdataLoader.correct);
         await nhdExpect(await loginForm.getPage()).toHaveNotHiddenSelector(accountNavigation.getContentsSelector());
     })
 
     test('Incorrect username or email', async ({loginForm}) => {
             
-        await steps(loginForm, incorrectUsernameOrEmail);
+        await steps(loginForm, await LoginTestdataLoader.incorrectUsername);
         await hdExpect(await loginForm.getPage()).toHaveHiddenSelector(accountNavigation.getContentsSelector());
         await nhdExpect(await loginForm.getPage()).toHaveNotHiddenSelector(loginForm.getErrorMessageSelector());
     })
 
     test('Blank "Username or email address" field',async ({loginForm}) => {
              
-        await steps(loginForm, blankUsernameOrEmailField);
+        await steps(loginForm, await LoginTestdataLoader.incorrectUsername);
         await hdExpect(await loginForm.getPage()).toHaveHiddenSelector(accountNavigation.getContentsSelector());
         await nhdExpect(await loginForm.getPage()).toHaveNotHiddenSelector(loginForm.getErrorMessageSelector());
     })
 
-    for(const data of incorrectPassword) {
-
-        test('Password: ' + data.password + '"', async ({loginForm}) => {
-            
-            await steps(loginForm, data);
+    test('Incorrect password',async ({loginForm}) => {
+        
+        await steps(loginForm, await LoginTestdataLoader.incorrectPassword);
             await hdExpect(await loginForm.getPage()).toHaveHiddenSelector(accountNavigation.getContentsSelector());
             await nhdExpect(await loginForm.getPage()).toHaveNotHiddenSelector(loginForm.getErrorMessageSelector());
-        })
-    }
+    })
+
+    test('Blank "Username or email" field',async ({loginForm}) => {
+             
+        await steps(loginForm, await LoginTestdataLoader.blankUsernameField);
+        await hdExpect(await loginForm.getPage()).toHaveHiddenSelector(accountNavigation.getContentsSelector());
+        await nhdExpect(await loginForm.getPage()).toHaveNotHiddenSelector(loginForm.getErrorMessageSelector());
+    })
 
     test('Blank "Password" field',async ({loginForm}) => {
              
-        await steps(loginForm, blankPasswordField);
+        await steps(loginForm, await LoginTestdataLoader.blankPasswordField);
         await hdExpect(await loginForm.getPage()).toHaveHiddenSelector(accountNavigation.getContentsSelector());
         await nhdExpect(await loginForm.getPage()).toHaveNotHiddenSelector(loginForm.getErrorMessageSelector());
     })
