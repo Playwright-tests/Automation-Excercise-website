5c14575dda6f86ed3bbcfc2a6bd51ddf9e0c6525
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-27 21:54:36 +0100

    Added the condition

--- services/dataService.ts
+++ services/dataService.ts
@@ -4,8 +4,15 @@ import { MockarooTestdataProvider } from "../testdata-providers/mockarooTestData
 export async function getTestdata(name: string) {
     
     try {
-        return await MockarooTestdataProvider.fetchData(name);
-    } catch {
+        const mockarooTestData = await MockarooTestdataProvider.fetchData(name);
+
+        if(!mockarooTestData) {
+            throw new Error('No testdata from Mockaroo')
+        }
+
+        return mockarooTestData;
+
+    } catch(error) {
         console.warn('Using the local testdata');
         return await LocalTestdataProvider.fetchData(name);
     }
