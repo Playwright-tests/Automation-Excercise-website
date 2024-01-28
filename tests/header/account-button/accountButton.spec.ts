515c68bb6ba62ad4186d0f5a7823d1e080d49d3a
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-28 09:01:32 +0100

    Added using the AccountPageLinkTestdataLoader

--- tests/header/account-button/accountButton.spec.ts
+++ tests/header/account-button/accountButton.spec.ts
@@ -1,20 +1,17 @@
 import { test, expect } from "../../../fixtures/header";
-import { getLinkData } from "../../../data-loaders/link";
+import { AccountPageLinkTestdataLoader } from "../../../data-loaders/dataLoaders";
 
-const links = getLinkData('accountPage');
+AccountPageLinkTestdataLoader.init();
 
 test.describe('Opening the "Account" page',async () => {
     
-    for(const data of links) {
-
-        test('Clicking the "Account" button',async ({header, page}) => {
+    test('Clicking the "Account" button',async ({header, page}) => {
             
-            await test.step('Click the "Account" button',async () => {
-                
-                await header.clickAccountButton();
-            })
-
-            await expect(page).toHaveURL(data.url);
+        await test.step('Click the "Account" button',async () => {
+            
+            await header.clickAccountButton();
         })
-    }
+
+        await expect(page).toHaveURL(AccountPageLinkTestdataLoader.link.url);
+    })
 })
