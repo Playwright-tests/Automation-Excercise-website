bf87fcc23f7ac25ed63ab468feee1e2686ad567d
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-09 23:14:08 +0100

    Changed the class to the function

--- helpers/accountService.ts
+++ helpers/accountService.ts
@@ -1,19 +1,16 @@
 import { Page } from "@playwright/test";
 import { LoginForm } from "../page-object/login-form/loginForm";
+import { CredentialsDataProvider } from "../data-loaders/dataProviders";
+import { TestScenarios } from "../enums/testScenarios";
 import { URLs } from "../enums/URLs";
-import { LoginTestdataLoader } from "../data-loaders/dataLoaders";
 
-export class AccountService {
+export async function login(page: Page) {
+    
+    const credentials = CredentialsDataProvider.get();
+    const loginForm = new LoginForm(page);
 
-    static async login(page: Page) {
-        
-        LoginTestdataLoader.init();
-
-        const loginForm = new LoginForm(page);
-
-        await loginForm.goto(URLs.LoginPage);
-        await loginForm.setUsername(await LoginTestdataLoader.correct.username);
-        await loginForm.setPassword(await LoginTestdataLoader.correct.password);
-        await loginForm.clickLoginButton();
-    }
+    await loginForm.goto(URLs.LoginPage);
+    await loginForm.setUsername(credentials[TestScenarios.CORRECT].emailOrUsername);
+    await loginForm.setPassword(credentials[TestScenarios.CORRECT].password);
+    await loginForm.clickLoginButton();
 }
