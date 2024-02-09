ece2d1a5eb22295472f52c9ea787bac8a21f6668
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-09 14:13:26 +0100

    Removed

--- data-loaders/JSONArray.ts
+++
@@ -1,9 +0,0 @@
-import { JSONReader } from "../json-reader/JSONReader";
-
-export function getJSONArray(key: string) {
-
-    let testdata = JSONReader.get();
-    let JSONArray = testdata[key];
-
-    return JSONArray;
-}
