29ac91bed6e905cc209dced8de180cf2490b5e49
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-09 12:39:51 +0100

    Added models

---
+++ models/models.ts
@@ -0,0 +1,25 @@
+export type LinkData = {
+
+    link: string,
+    url: string
+}
+
+export type Credentials = {
+
+    emailOrUsername: string,
+    password: string
+}
+
+export type AddressData = {
+
+    country: string,
+    firstName: string,
+    lastName: string,
+    company: string,
+    address: string,
+    addressLine2: string,
+    city: string,
+    postcode: string,
+    phone: string,
+    email: string
+}
