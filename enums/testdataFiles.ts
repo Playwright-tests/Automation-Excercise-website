174b244a4130968e7c9efbe5e95718f8ca71cf80
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-09 23:12:24 +0100

    Added file names

--- enums/testdataFiles.ts
+++ enums/testdataFiles.ts
@@ -1,6 +1,8 @@
 export enum TestdataFiles {
 
     LINKS = 'links',
+    PHRASES = 'GSP_Phrases.json',
+    COUNTRIES = 'GSP_Countries.json',
     CREDENTIALS = 'GSP_Credentials.json',
     ADDRESS = 'GSP_Address.json'
 }
