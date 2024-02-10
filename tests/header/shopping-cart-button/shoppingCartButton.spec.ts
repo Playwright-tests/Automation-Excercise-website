1735fb47e78f33e5f1cd3347e082cb0dea0135d5
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-10 18:22:43 +0100

    Removed the loop

--- tests/header/shopping-cart-button/shoppingCartButton.spec.ts
+++ tests/header/shopping-cart-button/shoppingCartButton.spec.ts
@@ -1,20 +1,15 @@
+import { URLs } from "../../../enums/URLs";
 import { test, expect } from "../../../fixtures/header";
-import { getLinkData } from "../../../data-loaders/link";
 
-const links = getLinkData('shoppingCartPage');
+test.describe('Opening the shopping cart page', async () => {
 
-test.describe('Opening the shopping cart page',async () => {
+    test('Clicking the "My Cart" button', async ({ header}) => {
 
-    for(const data of links) {
+        await test.step('Click the "My Cart" button', async () => {
 
-        test('Clicking the "My Cart" button',async ({header, page}) => {
-            
-            await test.step('Click the "My Cart" button',async () => {
-                
-                await header.clickShoppingCartButton();
-            })
-
-            await expect(page).toHaveURL(data.url);
+            await header.clickShoppingCartButton();
         })
-    }
+
+        await expect(await header.getPage()).toHaveURL(URLs.ShoppingCart);
+    })
 })
