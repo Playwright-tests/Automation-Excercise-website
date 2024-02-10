a765c47bdab50e9fa4ae70f0593856f8ae8abf42
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-10 18:21:37 +0100

    Added the function

--- tests/login-form/login.spec.ts
+++ tests/login-form/login.spec.ts
@@ -6,23 +6,26 @@ import { AccountNavigation } from "../../page-object/account/accountNavigation";
 import { Credentials } from "../../models/models";
 import { CredentialsDataProvider } from "../../data-loaders/dataProviders";
 import { TestScenarios } from "../../enums/testScenarios";
+import { LoginForm } from "../../page-object/login-form/loginForm";
 
+const credentials = CredentialsDataProvider.get();
 
 test.describe('Login',async () => {
 
-    let credentials: Credentials[];
     let accountNavigation: AccountNavigation;
-
-    test.beforeAll(async () => {
-        
-        credentials = CredentialsDataProvider.get();
-    })
 
     test.beforeEach(async ({page}) => {
         
         accountNavigation = new AccountNavigation(page);
     })
     
+    async function actionsForNegativeTests(loginForm: LoginForm, data: Credentials) {
+        
+        await steps(loginForm, data);
+        await hdExpect(await loginForm.getPage()).toHaveHiddenSelector(accountNavigation.getContentsSelector());
+        await nhdExpect(await loginForm.getPage()).toHaveNotHiddenSelector(loginForm.getErrorMessageSelector());
+    }
+
     test('Correct credentials',async ({loginForm}) => {
              
         await steps(loginForm, credentials[TestScenarios.CORRECT]);
@@ -30,30 +33,22 @@ test.describe('Login',async () => {
     })
 
     test('Incorrect username or email', async ({loginForm}) => {
-            
-        await steps(loginForm, credentials[TestScenarios.INCORRECT_EMAIL_OR_USERNAME]);
-        await hdExpect(await loginForm.getPage()).toHaveHiddenSelector(accountNavigation.getContentsSelector());
-        await nhdExpect(await loginForm.getPage()).toHaveNotHiddenSelector(loginForm.getErrorMessageSelector());
+          
+        await actionsForNegativeTests(loginForm, credentials[TestScenarios.INCORRECT_EMAIL_OR_USERNAME]);
     })
 
     test('Blank "Username or email address" field',async ({loginForm}) => {
              
-        await steps(loginForm, credentials[TestScenarios.BLANK_EMAIL_OR_USERNAME_FIELD]);
-        await hdExpect(await loginForm.getPage()).toHaveHiddenSelector(accountNavigation.getContentsSelector());
-        await nhdExpect(await loginForm.getPage()).toHaveNotHiddenSelector(loginForm.getErrorMessageSelector());
+        await actionsForNegativeTests(loginForm, credentials[TestScenarios.BLANK_EMAIL_OR_USERNAME_FIELD]);
     })
 
     test('Incorrect password',async ({loginForm}) => {
         
-        await steps(loginForm, credentials[TestScenarios.INCORRECT_PASSWORD]);
-        await hdExpect(await loginForm.getPage()).toHaveHiddenSelector(accountNavigation.getContentsSelector());
-        await nhdExpect(await loginForm.getPage()).toHaveNotHiddenSelector(loginForm.getErrorMessageSelector());
+        await actionsForNegativeTests(loginForm, credentials[TestScenarios.INCORRECT_PASSWORD]);
     })
 
     test('Blank "Password" field',async ({loginForm}) => {
              
-        await steps(loginForm, credentials[TestScenarios.BLANK_PASSWORD_FIELD]);
-        await hdExpect(await loginForm.getPage()).toHaveHiddenSelector(accountNavigation.getContentsSelector());
-        await nhdExpect(await loginForm.getPage()).toHaveNotHiddenSelector(loginForm.getErrorMessageSelector());
+        await actionsForNegativeTests(loginForm, credentials[TestScenarios.BLANK_PASSWORD_FIELD]);
     })
 })
