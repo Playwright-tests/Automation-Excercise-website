d87b0847e7130dd31df1a1bb986b75f5d9ee4d0f
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-25 12:29:51 +0100

    Removed

--- providers/thumbnails/thumbnailFactory.ts
+++
@@ -1,8 +0,0 @@
-import { Page } from "@playwright/test";
-import { ThumbnailCategory } from "../../enums/thumbnailCategory";
-import { Thumbnail } from "../../page-object/thumbnail/thumbnail";
-
-export interface ThumbnailFactory {
-
-    createThumbnail(page: Page, category: ThumbnailCategory, name: string) : Thumbnail;
-}
