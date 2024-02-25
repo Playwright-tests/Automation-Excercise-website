e3198bc17caff7971c67fb120c694aef0eab6971
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-25 15:25:26 +0100

    Added the function

--- tests/products-and-blogs-section/steps.spec.ts
+++ tests/products-and-blogs-section/steps.spec.ts
@@ -1,10 +1,21 @@
 import { Page, test } from "@playwright/test";
 import { ThumbnailCategory } from "../../enums/thumbnailCategory";
-import { ThumbnailFactory } from "../../providers/thumbnails/thumbnailFactory";
+import { BlogThumbnailProvider } from "../../support/blogThumbnailProvider";
+import { ProductThumbnailProvider } from "../../support/productThumbnailProvider";
 
-export async function steps(page: Page, category: ThumbnailCategory, factory: ThumbnailFactory, linkText: string) {
+export async function blogsSteps(page: Page, category: ThumbnailCategory, linkText: string) {
     
-    const thumbnail = factory.createThumbnail(page, category, linkText);
+    const thumbnail = BlogThumbnailProvider.create(page, category, linkText);
+
+    await test.step('Click the "' + linkText + '"',async () => {
+        
+        await thumbnail.clickLink();
+    })
+}
+
+export async function productsSteps(page: Page, tycheProduct: string, linkText: string) {
+    
+    const thumbnail = ProductThumbnailProvider.create(page, tycheProduct, linkText);
 
     await test.step('Click the "' + linkText + '"',async () => {
         
