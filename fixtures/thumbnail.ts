2d3bb28547b346989e1d0aa1ad1c0415e3915d50
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-25 13:57:41 +0100

    Added the fixture

--- fixtures/thumbnail.ts
+++ fixtures/thumbnail.ts
@@ -1,31 +1,36 @@
 import { test as base } from "@playwright/test";
-import { ThumbnailType } from "../enums/thumbnailType";
-import { ThumbnailProvider } from "../providers/thumbnails/thumbnailProvider";
-import { ThumbnailFactory } from "../providers/thumbnails/thumbnailFactory";
 import { URLs } from "../enums/URLs";
+import { ProductThumbnail } from "../page-object/thumbnail/productThumbnail";
+import { BlogThumbnail } from "../page-object/thumbnail/blogThumbnail";
 
 export { expect } from "@playwright/test";
 
 export type ProductThumbnailFixture = {
 
-    thumbnailFactory: ThumbnailFactory
+    productThumbnail: ProductThumbnail
 }
 
-export type ThumbnailTypeFixture = {
+export type BlogThumbnailFixture = {
 
-    thumbnailType: ThumbnailType
+    blogThumbnail: BlogThumbnail;
 }
 
 
-export const test = base.extend<ProductThumbnailFixture & ThumbnailTypeFixture>({
+export const test = base.extend<ProductThumbnailFixture & BlogThumbnailFixture>({
 
-    thumbnailType: [ThumbnailType.Product, {option: true}],
-
-    thumbnailFactory:async ({thumbnailType, page}, use) => {
+    productThumbnail:async ({page}, use) => {
         
-        const thumbnailFactory = ThumbnailProvider.getFactory(thumbnailType);
+        const productThumbnail = new ProductThumbnail(page);
 
-        await page.goto(URLs.HomePage);
-        await use(thumbnailFactory);
+        await productThumbnail.goto(URLs.HomePage);
+        await use(productThumbnail);
+    },
+
+    blogThumbnail:async ({page}, use) => {
+        
+        const blogThumbnail = new BlogThumbnail(page);
+
+        await blogThumbnail.goto(URLs.HomePage);
+        await use(blogThumbnail);
     }
 })
