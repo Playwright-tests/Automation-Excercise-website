29dc0ecd1d6f69b320f4b6231333adc618b031b7
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-25 12:44:28 +0100

    Removed configurations

--- fixtures/shoppingCart.ts
+++ fixtures/shoppingCart.ts
@@ -3,8 +3,6 @@ import { ShoppingCart } from "../page-object/shopping-cart/shoppingCart";
 import { ProductPage } from "../page-object/product-page/productPage";
 import { URLs } from "../enums/URLs";
 import { ThumbnailCategory } from "../enums/thumbnailCategory";
-import { ProductThumbnail } from "../page-object/thumbnail/productThumbnail";
-import { Product } from "../support/product";
 
 export { expect } from "@playwright/test";
 
@@ -35,51 +33,21 @@ export const test = base.extend<ShoppingCartFixture & ProductPageUrlFixture & Th
 
     productPageUrl: [URLs.AmariShirtProduct, {option: true}],
     thumbnailCategory: [ThumbnailCategory.AllBlackTops, {option: true}],
-    productName: ['Black Top', {option: true}],
+    productName: ['none', {option: true}],
 
-    shoppingCart:async ({page}, use) => {
+    shoppingCart:async ({page, productName}, use) => {
         
         const productPage = new ProductPage(page);
-
-        await productPage.goto(URLs.AmariShirtProduct);
-        await productPage.getQuantityField().setQuantity('1');
-        await productPage.clickAddToCartButton();
-
         const shoppingCart = new ShoppingCart(page);
 
-        await shoppingCart.goto(URLs.ShoppingCart);
+        if(productName !== "none") {
+
+            await productPage.goto(URLs.AmariShirtProduct);
+            await (await productPage.getQuantityField()).setQuantity('1');
+            await productPage.clickAddToCartButton();
+            await shoppingCart.goto(URLs.ShoppingCart);
+        }
+
         await use(shoppingCart);
-
-    },
-
-    afterAddingThroughThumbnailOnHomepage:async ({page, thumbnailFactory, thumbnailCategory}, use) => {
-        
-        const thumbnail = thumbnailFactory.createThumbnail(page, thumbnailCategory, "Black Top") as ProductThumbnail;
-
-        await thumbnail.goto(URLs.HomePage);
-
-        Product.init(await thumbnail.getLinkText(), await thumbnail.getPrice());
-
-        thumbnail.clickAddToCartButton();
-        await (await thumbnail.getPage()).waitForSelector(thumbnail.getViewCartButtonSelector());
-        await thumbnail.goto(URLs.ShoppingCart);
-
-        const afterAddingThroughProductPage = new ShoppingCart(page);
-
-        await use(afterAddingThroughProductPage);
-    },
-
-    afterAddingThroughProductPage:async ({page, productPageUrl}, use) => {
-      
-        const productPage = new ProductPage(page);
-        await productPage.goto(productPageUrl);
-        await productPage.getQuantityField().setQuantity('1');
-
-        Product.init(await productPage.getProductTitle(), await productPage.getProductPrice());
-
-        await productPage.clickAddToCartButton();
-        const afterAddingThroughProductPage = new ShoppingCart(page);
-        await afterAddingThroughProductPage.goto(URLs.ShoppingCart);
-        await use(afterAddingThroughProductPage);
-    },
+    }
 })
