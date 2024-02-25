da1705e5dca68abb19821c54d7e640553638310a
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-25 12:35:14 +0100

    Added fields

--- enums/testdataFiles.ts
+++ enums/testdataFiles.ts
@@ -6,5 +6,7 @@ export enum TestdataFiles {
     CREDENTIALS = 'GSP_Credentials.json',
     ADDRESS = 'GSP_Address.json',
     DYNAMIC_FIELDS = "GSP_DynamicFields.json",
-    NEWSLETTER = 'GSP_Newsletter.json'
+    NEWSLETTER = 'GSP_Newsletter.json',
+    PRODUCTS_TO_CART = 'GSP_ProductsToCart.json',
+    THUMBNAIL_DATA = 'GSP_Thumbnails.json'
 }
