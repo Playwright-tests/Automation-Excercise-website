e5ba3c35d0629b990f8a777375fa06a5182733dc
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-25 12:36:48 +0100

    Added types

--- models/models.ts
+++ models/models.ts
@@ -29,3 +29,22 @@ export type NewsletterData = {
     name: string,
     email: string
 }
+
+export type ProductToCart = {
+
+    url: string,
+    quantity: string
+}
+
+export type ProductData = {
+
+    name: string | null;
+    price: string | null;
+    quantity: string | null;
+}
+
+export type ThumbnailData = {
+
+    tycheProduct: string,
+    link: string;
+}
