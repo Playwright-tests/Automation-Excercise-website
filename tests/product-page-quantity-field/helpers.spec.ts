5d5dc08f09bd8c656b91e0bb65349b5ec5ec4c2f
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-26 18:16:58 +0100

    Removed

--- tests/product-page-quantity-field/helpers.spec.ts
+++
@@ -1,9 +0,0 @@
-import { ProductPage } from "../../page-object/product-page/productPage";
-
-export async function getExpectedMessage(productPage: ProductPage) {
-    
-    const begin = await productPage.getQuantityField().getQuantity() === '1' ? '' : ' ' + await productPage.getQuantityField().getQuantity() + ' ×';
-    const verb = await productPage.getQuantityField().getQuantity() === '1' ? 'has' : 'have';
-
-    return 'View cart' + begin + ' “' + await productPage.getProductTitle() + '” ' + verb + ' been added to your cart.';
-}
