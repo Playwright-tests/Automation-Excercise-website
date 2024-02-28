1ae669eeed5e6658e8d5b6759d5c52612ad9c3d8
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-28 18:22:49 +0100

    Added the fields

--- enums/testdataFiles.ts
+++ enums/testdataFiles.ts
@@ -8,5 +8,6 @@ export enum TestdataFiles {
     DYNAMIC_FIELDS = "GSP_DynamicFields.json",
     NEWSLETTER = 'GSP_Newsletter.json',
     PRODUCTS_TO_CART = 'GSP_ProductsToCart.json',
-    THUMBNAIL_DATA = 'GSP_Thumbnails.json'
+    THUMBNAIL_DATA = 'GSP_Thumbnails.json',
+    NEWSLETTER_FIELDS = 'GSP_NewsletterFields.json'
 }
