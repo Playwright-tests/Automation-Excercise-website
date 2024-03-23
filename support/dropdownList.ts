efd8cc437b4136f14233e962558fc17b7bfcee30
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-23 12:48:57 +0100

    Renamed the method

--- support/dropdownList.ts
+++ support/dropdownList.ts
@@ -1,6 +1,6 @@
 import { test, expect, Page } from "@playwright/test";
 
-export async function dropdownListAction(page: Page, linkName: string, expectedURL: string, func: any) {
+export async function menuAction(page: Page, linkName: string, expectedURL: string, func: any) {
     
     await test.step('Click the "' + linkName + '" link',async () => {
         await func();
