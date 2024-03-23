c57d59415bf79ea2cc4a284f1105c5d5656b897f
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-23 07:49:35 +0100

    Added methods

--- page-object/main-menu/mainMenu.ts
+++ page-object/main-menu/mainMenu.ts
@@ -8,13 +8,43 @@ export class MainMenu extends BasePage {
         super(page);
     }
 
-    async clickHomeButton() {
+    async clickHomeLink() {
 
-        await page.getByRole('link', {name: 'Home'}).click();
+        await (await this.getPage()).getByRole('link', {name: 'Home'}).click();
     }
 
-    async clickProductsButton() {
+    async clickProductsLink() {
 
-        await page.getByRole('link', {name: 'Products'}).click();
+        await (await this.getPage()).getByRole('link', {name: 'Products'}).click();
+    }
+
+    async clickCartLink() {
+
+        await (await this.getPage()).getByRole('link', {name: 'Cart'}).click();
+    }
+
+    async clickSignupLink() {
+
+        await (await this.getPage()).getByRole('link', {name: 'Signup / Login'}).click();
+    }
+
+    async clickTestCasesLink() {
+
+        await (await this.getPage()).getByRole('link', {name: ' Test Cases'}).click();
+    }
+
+    async clickAPITestingLink() {
+
+        await (await this.getPage()).getByRole('link', {name: 'API Testing'}).click();
+    }
+
+    async clickVideoTutorialsLink() {
+
+        await (await this.getPage()).getByRole('link', {name: 'Video Tutorials'}).click();
+    }
+
+    async clickContactUs() {
+
+        await (await this.getPage()).getByRole('link', {name: 'Contact us'}).click();
     }
 }
