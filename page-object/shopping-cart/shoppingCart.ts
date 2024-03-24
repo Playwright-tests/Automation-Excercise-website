bbbc2f556710f523f1c4c2a5aea8c0d3538e51b7
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-24 22:40:44 +0100

    Removed methods

--- page-object/shopping-cart/shoppingCart.ts
+++ page-object/shopping-cart/shoppingCart.ts
@@ -1,73 +1,16 @@
 import { Locator, Page } from "@playwright/test";
 import { BasePage } from "../base/BasePage";
-import { ShoppingCartProcessing } from "./shoppingCartProcessing";
 import { Table } from "./table";
 
 export class ShoppingCart extends BasePage {
 
-    readonly messageSelector: string;
-    readonly tableSelector = '.shop_table.shop_table_responsive.cart.woocommerce-cart-form__contents'
-    readonly cartEmpty: Locator;
-    readonly updateButton: Locator;
-    readonly message: Locator;
-    readonly checkoutButton: Locator;
-
-    private table: Table;
-    private shoppingCartProcessing: ShoppingCartProcessing;
+    private readonly table: Table;
 
     constructor(page: Page) {
 
         super(page);
 
-        this.messageSelector = '.woocommerce-message';
-
-        this.cartEmpty = page.getByText('Your cart is currently empty.');
-        this.updateButton = page.getByRole('button', {name: 'Update cart'});
-        this.message = page.locator(this.messageSelector);
-        this.checkoutButton = page.getByRole('link', {name: 'Proceed to checkout'});
-
         this.table = new Table(page);
-        this.shoppingCartProcessing = new ShoppingCartProcessing(page);
-    }
-
-    getCartEmptyLocator() {
-
-        return this.cartEmpty;
-    }
-
-    async clickUpdateButton() {
-
-        await this.updateButton.click();
-    }
-
-    async clickCheckoutButton() {
-
-        await this.checkoutButton.click();
-    }
-
-    async getMessageText() {
-
-        return await this.message.textContent();
-    }
-
-    getMessageLocator() {
-
-        return this.message;
-    }
-
-    getMessageSelector() {
-
-        return this.messageSelector;
-    }
-
-    getTableSelector() {
-
-        return this.tableSelector;
-    }
-
-    getShoppingCartProcessing() {
-
-        return this.shoppingCartProcessing;
     }
 
     getTable() {
