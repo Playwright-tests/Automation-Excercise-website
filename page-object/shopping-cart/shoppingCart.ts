e2e07c1860f2d00853c0a0271a34ee953689f4aa
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-25 17:56:25 +0100

    Added methods

--- page-object/shopping-cart/shoppingCart.ts
+++ page-object/shopping-cart/shoppingCart.ts
@@ -1,20 +1,35 @@
 import { Locator, Page } from "@playwright/test";
 import { BasePage } from "../base/BasePage";
 import { Table } from "./table";
+import { CheckoutModalDialog } from "./checkoutModalDialog";
 
 export class ShoppingCart extends BasePage {
 
     private readonly table: Table;
+    private readonly CHECKOUT_BUTTON: Locator;
+    private readonly CHECKOUT_MODAL_DIALOG: CheckoutModalDialog;
 
     constructor(page: Page) {
 
         super(page);
 
         this.table = new Table(page);
+        this.CHECKOUT_BUTTON = page.getByText('Proceed To Checkout');
+        this.CHECKOUT_MODAL_DIALOG = new CheckoutModalDialog(page);
     }
 
     getTable() {
 
         return this.table;
+    }
+
+    async clickCheckoutButton() {
+
+        await this.CHECKOUT_BUTTON.click();
+    }
+
+    getCheckoutModalDialog() {
+
+        return this.CHECKOUT_MODAL_DIALOG;
     }
 }
