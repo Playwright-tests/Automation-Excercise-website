ab8d7f0f5c30774caece58a683b4ad6f0ecf9b41
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-25 11:45:48 +0100

    Added the function

--- testdata-IO/testdataLoader.ts
+++ testdata-IO/testdataLoader.ts
@@ -1,7 +1,15 @@
+import { TestDataFileNames } from "../enums/testdataFileNames";
+import { Credentials } from "../types/types";
 import { TestdataReader } from "./testdataReader";
 
 export function getStrings(fileName: string, key: string): string[] {
 
     let temp = TestdataReader.get(fileName);
+    return temp[key];
+}
+
+export function getCredentials(key: string): Credentials[] {
+
+    let temp = TestdataReader.get(TestDataFileNames.CREDENTIALS);
     return temp[key];
 }
