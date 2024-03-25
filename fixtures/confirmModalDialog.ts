fc349cfb78f67e66a7a0ad0acb2ef3348ea87d73
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-25 17:55:27 +0100

    Renamed the fixture

--- fixtures/confirmModalDialog.ts
+++ fixtures/confirmModalDialog.ts
@@ -1,27 +1,24 @@
 import { test as base } from "@playwright/test";
-import { ConfirmModalDialog } from "../page-object/confirm-modal-dialog/confirmModalDialog";
 import { ProductPage } from "../page-object/product-page/productPage";
 
 export { expect } from "@playwright/test";
 
-export const test = base.extend<{url: string} & {hidden: ConfirmModalDialog} & {visible: ConfirmModalDialog}>({
+export const test = base.extend<{url: string} & {hidden: ProductPage} & {visible: ProductPage}>({
 
     url: ['noUrl', {option: true}],
 
     hidden:async ({page, url}, use) => {
         
-        const productPage = new ProductPage(page);
-        const hidden = new ConfirmModalDialog(page);
-        await productPage.goto(url);
+        const hidden = new ProductPage(page);
+        await hidden.goto(url);
         await use(hidden);
     },
 
     visible:async ({page, url}, use) => {
         
-        const productPage = new ProductPage(page);
-        const visible = new ConfirmModalDialog(page);
-        await productPage.goto(url);
-        await productPage.clickAddToCartButton();
+        const visible = new ProductPage(page);
+        await visible.goto(url);
+        await visible.clickAddToCartButton();
         await use(visible)
     }
 })
