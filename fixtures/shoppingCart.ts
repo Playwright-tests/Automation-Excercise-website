cf1a731d0970f4b5cde73418dab89ec2f12b82c8
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-25 21:48:09 +0100

    Added the fixture

--- fixtures/shoppingCart.ts
+++ fixtures/shoppingCart.ts
@@ -1,13 +1,25 @@
 import { test as base } from "@playwright/test";
 import { ShoppingCart } from "../page-object/shopping-cart/shoppingCart";
+import { fillShoppingCart } from "../support/shoppingCartFilling";
+import { URLs } from "../enums/URLs";
+import { authentication } from "../support/authentication";
 
 export { expect } from "@playwright/test";
 
-export const test = base.extend<{empty: ShoppingCart}>({
+export const test = base.extend<{empty: ShoppingCart} & {authenticated: ShoppingCart}>({
 
     empty:async ({page}, use) => {
         
         const empty = new ShoppingCart(page);
         await use(empty);
+    },
+
+    authenticated:async ({page}, use) => {
+        
+        const authenticated = new ShoppingCart(page);
+        await authentication(page);
+        await fillShoppingCart(page);
+        await page.goto(URLs.SHOPPING_CART_PAGE);
+        await use(authenticated);
     }
 }) 
