e04f9ddc294b6ab25962833ff71daeb3a056980b
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-25 11:44:56 +0100

    Added the field

--- enums/testdataFileNames.ts
+++ enums/testdataFileNames.ts
@@ -3,5 +3,6 @@ export enum TestDataFileNames {
     SUBSCRIPTION = 'AE_Subscription.json',
     PHRASES = 'AE_Phrases.json',
     PRODUCT_PAGE_URLS = 'AE_ProductURLs.json',
-    INCORRECT_QUANTITIES = 'AE_IncorrectQuantities.json'
+    INCORRECT_QUANTITIES = 'AE_IncorrectQuantities.json',
+    CREDENTIALS = 'AE_Credentials.json'
 }
