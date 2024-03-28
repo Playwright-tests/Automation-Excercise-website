4d9a9ba0756e16f0fa1dfd51b1eb2446e6b69da7
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-28 08:08:58 +0100

    Added the type

--- types/types.ts
+++ types/types.ts
@@ -10,3 +10,12 @@ export type Credentials = {
     email: string,
     password: string
 }
+
+export type CreditCardData = {
+
+    brand: string,
+    number: string,
+    cvc: string,
+    month: string,
+    year: string
+}
